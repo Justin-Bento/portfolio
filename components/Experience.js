f99dfb7e2b42e840a62614466d3ba9421f1f442f
Justin-Bento <bentoj005@outlook.com>
2021-03-21 19:45:35 -0400

    created an experience component with props

---
+++ components/Experience.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class Experience extends Component {
+  render() {
+    return (
+      <div className="Experience mb-8">
+        <h4 className="text-xl text-gray-900 font-semibold capitalize mb-1">{this.props.Title}</h4>
+        <p className="text-base text-gray-900 capitalize">{this.props.EmploymentType}</p>
+        <p className="text-base text-gray-900 capitalize">{this.props.StartEndDate}</p>
+        <p className="text-base text-gray-900 mb-1 capitalize">{this.props.Location}</p>
+        <p className="text-gray-900 text-lg">{this.props.Description}</p>
+      </div>
+    );
+  }
+}
+export { Experience };
