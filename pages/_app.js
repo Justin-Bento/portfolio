13aecf3d568fed676795f44c868188044314acfc
Justin-Bento <bentoj005@outlook.com>
2021-03-21 23:37:41 -0400

    added next head globaly

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
 import { Appbar } from "../components/Appbar";
+import Head from "next/head";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <title>Justin Bento • Front-end Designer</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta charset="UTF-8" />
+        <meta
+          name="description"
+          content="Hi, I’m Justin Bento. I am a  UI/UX designer in Canada. I enjoy helping business evolve by solving problems that enhance your brand and drive innovation."
+        />
+        <meta property="og:image" content="/website-meta.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
       <header>
         <nav>
           <Appbar />
