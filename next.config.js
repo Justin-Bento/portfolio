83cfbdd4c4e721a8f8c47ad2430c1bc92600d9d1
Justin-Bento <bentoj005@outlook.com>
2021-03-23 09:57:08 -0400

    added  lang attribute to html

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   // Target must be serverless
   target: "serverless",
+  // Internationalized Language Support
+  i18n: {
+    locales: ["en-US", "fr", "nl-NL"],
+    defaultLocale: "en-US",
+  },
 };
