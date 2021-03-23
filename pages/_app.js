a2b33f8c81ad2e6585953fcbeee630e072691e8f
Justin-Bento <bentoj005@outlook.com>
2021-03-23 09:47:47 -0400

    preloaded serif font

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,8 @@ function MyApp({ Component, pageProps }) {
         />
         <meta property="og:image" content="/website-meta.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="preload" as="font" type="font/ttf" href="/fonts/goudy/goudy_bookletter_191.ttf" crossorigin />
       </Head>
 
       <header>
