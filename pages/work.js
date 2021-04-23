1c1615ab913576aad0810a2c243a41621979ddee
Justin-Bento <bentoj005@outlook.com>
2021-04-23 04:25:39 -0400

    updated typography and spacing

--- pages/work.js
+++ pages/work.js
@@ -3,6 +3,13 @@ import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
 import { getWorkPosts } from '../lib/work-data';
 
+let headline_primary = 'text-4xl md:text-5xl font-bold mb-4';
+let headline_secondary = 'mb-6 text-3xl font-bold';
+let headline_tertiary = 'my-2 font-semibold text-lg';
+let body_lg = 'prose-xl pr-5';
+let body = 'prose 2xl:prose-xl';
+let body_sm = 'my-1 prose-sm 2xl:prose-xl';
+
 export default function work({ posts }) {
   return (
     <div>
@@ -13,8 +20,8 @@ export default function work({ posts }) {
 
       <main>
         <div className="my-8">
-          <div className="px-4 py-8 mx-auto max-w-7xl">
-            <div className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+          <div className="mx-auto max-w-6xl">
+            <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
               {posts.map((item) => (
                 <WorkListItem key={item.slug} {...item} />
               ))}
@@ -47,22 +54,22 @@ function WorkListItem({ slug, title, date, content, media, mediaAlt, type }) {
         <Link href={`/work/${slug}`}>
           <img
             src={`/img/work/${media}`}
-            class="object-cover w-full h-56 mb-3 bg-center rounded"
+            class="object-cover w-full h-56 bg-center rounded-lg my-2"
             alt={mediaAlt}
             loading="lazy"
           />
         </Link>
       </div>
-      <h2 className="mb-1 text-lg font-semibold text-gray-900">
+      <h2 className={headline_tertiary}>
         <Link href={`/work/${slug}`}>
-          <a className="text-gray-900 hover:text-green-700">{title}</a>
+          <a className="text-gray-900 hover:text-green-600">{title}</a>
         </Link>
       </h2>
       <div className="description">
         <Link href={`/work/${slug}`}>
           <a>
-            <p className="mb-3 text-sm font-normal text-gray-500">{content.substr(0, 200)}</p>
-            <p className="mb-3 text-sm font-normal text-gray-500">
+            <p className={body_sm}>{content.substr(0, 200)}</p>
+            <p className={body_sm}>
               {type} &sdot; {format(parseISO(date), 'MMMM, uuu')}
             </p>
           </a>
