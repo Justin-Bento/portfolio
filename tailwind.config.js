4e620dc8964c9370a6bb20a00499a261a8c1b2c2
Justin-Bento <bentoj005@outlook.com>
2021-04-24 15:16:31 -0400

    replaced double qotes with single quotes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Public Sans"', "system-ui", "-apple-system", "sans-serif", '"Apple Color Emoji"'],
-        serif: ['"Goudy Bookletter 1911"', "Cambria", '"Times New Roman"', "Times", "serif"],
+        sans: ['"Public Sans"', 'system-ui', '-apple-system', 'sans-serif', '"Apple Color Emoji"'],
+        serif: ['"Goudy Bookletter 1911"', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
       },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')],
 };
