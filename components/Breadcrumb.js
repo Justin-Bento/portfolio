e96be64efb2e85b5a06f2e23c5e7600bfa92b245
Justin-Bento <bentoj005@outlook.com>
2021-04-24 11:08:02 -0400

    updated colour for active breadcrub

--- components/Breadcrumb.js
+++ components/Breadcrumb.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class Breadcrumb extends Component {
   render() {
     return (
       <div className="Breadcrumb">
-        <ol className="flex leading-none divide-x divide-indigo-400">
+        <ol className="flex leading-none divide-x divide-green-500">
           <li className="pr-4">
             <a href={this.props.Link}>{this.props.Name}</a>
           </li>
-          <li className="px-4 text-blue-700" aria-current="page">
+          <li className="px-4 text-green-600" aria-current="page">
             {this.props.PathName}
           </li>
         </ol>
