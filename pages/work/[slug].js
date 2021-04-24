89a5121c7d308bee17aaa74af69682e1abd46646
Justin-Bento <bentoj005@outlook.com>
2021-04-24 17:41:18 -0400

    added hero image with slug file

--- pages/work/[slug].js
+++ pages/work/[slug].js
@@ -1,13 +1,13 @@
-import Head from "next/head";
-import { format, parseISO } from "date-fns";
-import renderToString from "next-mdx-remote/render-to-string";
-import hydrate from "next-mdx-remote/hydrate";
-import { Article } from "../../components/Article";
-import { Breadcrumb } from "../../components/Breadcrumb";
+import Head from 'next/head';
+import { format, parseISO } from 'date-fns';
+import renderToString from 'next-mdx-remote/render-to-string';
+import hydrate from 'next-mdx-remote/hydrate';
+import { Article } from '../../components/Article';
+import { Breadcrumb } from '../../components/Breadcrumb';
 
-import { getWorkPosts } from "../../lib/work-data";
+import { getWorkPosts } from '../../lib/work-data';
 
-export default function Work({ title, date, content, description }) {
+export default function Work({ title, date, content, description, media }) {
   const hydratedContent = hydrate(content);
 
   return (
@@ -17,11 +17,15 @@ export default function Work({ title, date, content, description }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="w-11/12 my-16 mx-auto sm:max-w-4xl">
-        <div className="work-post">
-          <Breadcrumb Name="Work" Link="/work" PathName={title} />
-          <Article Headline={title} Meta={format(parseISO(date), "MMMM do, uuu")} Subtitle={description} />
-          <div className="prose my-8 sm:max-w-5xl">{hydratedContent}</div>
+      <main>
+        <img src={`/img/work/${media}`} alt="" className="w-full h-80 mb-16 object-cover" />
+        <div className="w-11/12  mx-auto sm:max-w-4xl">
+          <div className="work-post">
+            <Breadcrumb Name="Work" Link="/work" PathName={title} />
+            <Article Headline={title} Meta={format(parseISO(date), 'MMMM do, uuu')} Subtitle={description} />
+            <div className="prose my-8 sm:max-w-5xl">{hydratedContent}</div>
+            {/* end  of wrapper */}
+          </div>
         </div>
       </main>
     </div>
