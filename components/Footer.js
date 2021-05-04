28bccb110e8e2a537db85e61df44912f97ee983d
Justin-Bento <bentoj005@outlook.com>
2021-05-04 14:30:45 -0400

    updated footer colour scheme

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
 let headline_primary = 'text-4xl md:text-4xl font-bold mb-4';
-let headline_secondary = 'mb-4 text-3xl font-bold capitalize';
+let headline_secondary = 'mb-4 text-3xl font-bold capitalize text-gray-100';
 let headline_tertiary = 'mb-1 font-semibold text-lg';
-let body_lg = 'prose-xl pr-5 text-gray-600';
-let body = 'prose 2xl:prose-xl';
+let body_lg = 'prose-xl pr-5 text-gray-100';
+let body = 'prose 2xl:prose-xl text-gray-100';
 
 export default class Footer extends Component {
   render() {
     return (
       <div>
         <div className="Footer mt-24">
-          <div className="bg-gray-100 py-12">
+          <div className="bg-green-600 py-12">
             <div className="w-11/12 mx-auto md:max-w-6xl">
               <h2 className={headline_secondary}>Let’s Work Together</h2>
               <p className={body}>
@@ -19,7 +19,7 @@ export default class Footer extends Component {
                 collaberate with people.
               </p>
               <button
-                className="flex items-center h-10 mt-8 px-4 text-gray-50 duration-150 bg-gray-600 rounded focus:shadow-outline hover:bg-gray-700"
+                className="flex items-center h-11 mt-8 px-4 bg-green-50 rounded-sm hover:bg-green-100 hover:text-green-900 transition-colors delay-75"
                 onClick={() => navigator.clipboard.writeText('hello@justinbento.ca')}
               >
                 hello@justinbento.ca
