bf36053069fa9333776a17f5a8fecd7344586013
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-02-12 23:41:23 -0500

    Resolved color contrast issue - adjusted colors to WCAG 2.1 standards.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Example() {
     <footer className="">
       <div className="p-6 mx-auto max-w-7xl">
         <div className="mt-8 md:order-1 md:mt-0">
-          <p className="items-start font-sans text-sm leading-5 text-gray-500 md:text-center">
+          <p className="items-start font-sans text-sm leading-5 text-[#30302E] md:text-center">
             &copy; {new Date().getFullYear()} Justin Bento, Inc. All rights reserved.
           </p>
         </div>
