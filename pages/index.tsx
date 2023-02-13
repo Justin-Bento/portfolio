bc97384e60c9914bcf51dc3c6b18aeaff4dfdf0c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-02-12 23:28:25 -0500

    Added SEO optimizations - included website title and description tags to improve visibility

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        {/* <!-- Primary Meta Tags --> */}
+        <title>Justin Bento Portfolio</title>
+        <meta name="title" content="Justin Bento Portfolio" />
+        <meta name="description" content="Join me, on this adventure through my portfolio. Experience my passion for design, dev, and music all located  in one convenient place." />
+      </Head>
       <Header />
       <main className="p-4 my-12 md:my-24">
         <section className="grid items-center grid-cols-1 gap-8 lg:grid-cols-2">
