6292f82da1770fb08e988a1c60f62d699a5477fb
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-02-12 22:56:14 -0500

    Updated Header & Footer design.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,13 @@
-export default function Footer() {
+export default function Example() {
   return (
-    <>
-    <footer className="p-4">
+    <footer className="">
+      <div className="p-6 mx-auto max-w-7xl">
+        <div className="mt-8 md:order-1 md:mt-0">
+          <p className="items-start font-sans text-sm leading-5 text-gray-500 md:text-center">
+            &copy; {new Date().getFullYear()} Justin Bento, Inc. All rights reserved.
+          </p>
+        </div>
+      </div>
     </footer>
-  </>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,18 +3,35 @@ import Link from "next/link";
 export default function Header() {
   return (
     <>
-      <header className="grid grid-cols-1 gap-8 p-4 lg:grid-cols-2">
-        <section className="flex items-center justify-between">
-          <p className="text-3xl font-display">JB</p>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path></svg>
-        </section>
-        <nav className="flex-col hidden gap-8 md:inline-flex">
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/projects">Projects</Link>
-          <Link href="/uses">Uses</Link>
-          <Link href="/contact">Contact</Link>
+      <header className="">
+        <nav className="px-2 sm:px-4 py-2.5 rounded">
+          <div className="flex flex-wrap items-center justify-between">
+            <a href="https://flowbite.com/" className="flex items-center">
+              <span className="self-center text-xl font-semibold whitespace-nowrap font-display">JB</span>
+            </a>
+            <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
+              <span className="sr-only">Open main menu</span>
+              <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            </button>
+            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+              <ul className="flex flex-col p-4 mt-4 border md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 ">
+                <li>
+                  <Link href="/" className="font-sans block py-2 pl-3 pr-4 text-[#443733] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#B45720] md:p-0" aria-current="page">Home</Link>
+                </li>
+                <li>
+                  <Link href="/about" className="font-sans block py-2 pl-3 pr-4 text-[#443733] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#B45720] md:p-0">About</Link>
+                </li>
+                <li>
+                  <Link href="/projects" className="font-sans block py-2 pl-3 pr-4 text-[#443733] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#B45720] md:p-0">Projects</Link>
+                </li>
+                <li>
+                  <Link href="/contact" className="font-sans block py-2 pl-3 pr-4 text-[#443733] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#B45720] md:p-0">Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
+
       </header>
     </>
   )
