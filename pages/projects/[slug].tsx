d5323a620bdcd9e759808db8ed24cb2ca6d32dbf
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-02-13 02:48:19 -0500

    created a dynamic page to render additional conotent

---
+++ pages/projects/[slug].tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default function ProjectsSlug() {
+  return (
+    <>
+      <Header />
+      <main className="">
+
+      </main>
+      <Footer />
+    </>
+  )
+}
