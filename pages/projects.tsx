574614295097c96b83eb14cab32c948434c11e44
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-02-14 12:15:15 -0500

    included recent projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -7,16 +7,19 @@ export default function projects() {
   return (
     <>
       <Head>
-        <title></title>
+        {/* <!-- Primary Meta Tags --> */}
+        <title>Projects - Justin Bento Portfolio</title>
+        <meta name="title" content="Justin Bento Portfolio" />
+        <meta name="description" content="Join me, on this adventure through my portfolio. Experience my passion for design, dev, and music all located  in one convenient place." />
       </Head>
       <Header />
       <main className="">
         <section className="p-4 my-12 md:my-24">
           <h1 className="text-5xl leading-relaxed capitalize lg:leading-[7rem] lg:text-8xl font-display text-[#443733]">Things I’ve made trying to put my dent in the space.</h1>
         </section>
-        <section className="space-y-24">
+        <section className="flex flex-col gap-24">
           {project_history.map((data: any) => {
-            return <Project_Card key={data.title.toString()} Link={`projects/${data.title.toLowerCase().replace(/ /g, '-')}`} Headline={data.title} />
+            return <Project_Card key={data.title.toString()} Link={`projects/${data.title.toLowerCase().replace(/ /g, '-')}`} Headline={data.title} Media={data.Image} />
           })}
         </section>
       </main>
@@ -26,11 +29,10 @@ export default function projects() {
 }
 
 const project_history = [
-  {title: "Paramount Properties LTDs"},
-  {title: "Belive Support Services"},
-  {title: "Option One Staffing Agency"},
-  {title: "Portfolio Website"},
-  {title: "Marker"},
-  {title: "Redesign: Antigua News Room"},
-  {title: "ParkSmart"},
+  { title: "Paramount Properties LTD", Image: "/media/websites/paramount-properties-ltd.png" },
+  { title: "Belive Support Services", Image: "/media/websites/belive-support-services.png" },
+  { title: "Option One Staffing Agency", Image: "/media/websites/option-one-staffing-agency.png" },
+  { title: "Portfolio Website", Image: "/media/websites/justin-bento.png" },
+  { title: "Urban Greens Antigua", Image: "/media/websites/urban-greens-antigua.png"  },
+  { title: "Redesign: Antigua News Room", Image: "/media/websites/antigua-newsroom-redesign.png" },
 ]
