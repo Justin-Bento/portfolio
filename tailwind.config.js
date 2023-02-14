8468e322b11d2a8dd607a8a6cc376271d1cef39d
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-02-14 14:27:03 -0500

    included brand colors to use throughout the main project

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,21 @@ module.exports = {
         display: ['Julee', ...defaultTheme.fontFamily.sans],
         sans: ['Epilogue', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        primary: {
+          '50': '#f8f6f4',
+          '100': '#ece6e1',
+          '200': '#ddd3cb',
+          '300': '#c8b5a9',
+          '400': '#b19486',
+          '500': '#a17d6e',
+          '600': '#946d62',
+          '700': '#7b5953',
+          '800': '#654a47',
+          '900': '#533e3b',
+      },
+      
+      }
     },
   },
   plugins: [],
