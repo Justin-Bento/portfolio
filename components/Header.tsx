101b1f35852b297c0a490cb562e0235ee315eba7
Justin Bento <bento.a.justin@gmail.com>
2023-02-24 12:33:14 -0500

    updated navigation components with tailwindui

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,41 +1,95 @@
-import Link from "next/link";
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { HiMenu, HiX } from 'react-icons/hi'
 
-export default function Header() {
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+]
+
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
   return (
-    <>
-      <header className="">
-        <nav className="px-2 sm:px-4 py-2.5 rounded">
-          <div className="flex flex-wrap items-center justify-between">
-            <Link href="/" aria-label="home-page-internal-link" className="flex items-center">
-              <span className="self-center text-xl font-semibold whitespace-nowrap font-display">JB</span>
-            </Link>
-            <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 ">
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+    <header className="">
+      <nav className="flex items-center justify-between p-6 mx-auto max-w-7xl lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img className="w-auto h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <HiMenu className="w-6 h-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map((item) => (
+            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+              {item.name}
+            </a>
+          ))}
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            View Resume
+          </a>
+        </div>
+      </nav>
+      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full px-6 py-6 overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                className="w-auto h-8"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt=""
+              />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <HiX className="w-6 h-6" aria-hidden="true" />
             </button>
-            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-              <ul className="flex flex-col p-4 mt-4 border md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 ">
-                <li>
-                  <Link aria-label="home-page-internal-link" href="/" className="font-sans block py-2 pl-3 pr-4 text-[#443733] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#B45720] md:p-0">Home</Link>
-                </li>
-                <li>
-                  <Link aria-label="about-page-internal-link" href="/about" className="font-sans block py-2 pl-3 pr-4 text-[#443733] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#B45720] md:p-0">About</Link>
-                </li>
-                <li>
-                  <Link aria-label="project-page-internal-link" href="/projects" className="font-sans block py-2 pl-3 pr-4 text-[#443733] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#B45720] md:p-0">Projects</Link>
-                </li>
-                <li>
-                  <Link aria-label="uses-page-internal-link" href="/uses" className="font-sans block py-2 pl-3 pr-4 text-[#443733] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#B45720] md:p-0">Uses</Link>
-                </li>
-                <li>
-                  <Link aria-label="contact-page-internal-link" href="/contact" className="font-sans block py-2 pl-3 pr-4 text-[#443733] rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#B45720] md:p-0">Contact</Link>
-                </li>
-              </ul>
+          </div>
+          <div className="flow-root mt-6">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="py-6 space-y-2">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="block px-3 py-2 -mx-3 text-base font-semibold leading-7 text-gray-900 rounded-lg hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  View Resume
+                </a>
+              </div>
             </div>
           </div>
-        </nav>
-
-      </header>
-    </>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
   )
 }
