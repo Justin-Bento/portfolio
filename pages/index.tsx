dcacf6f19ca6d78b0b85e9d461d4f82173c2e4e4
Justin Bento <bento.a.justin@gmail.com>
2023-02-24 15:34:04 -0500

    updated links to projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import Footer from "@/components/Footer";
 import Project_Card from "@/components/Project_Card";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
+import { HiExternalLink } from 'react-icons/hi'
+
 
 export default function index() {
   return (
@@ -86,23 +89,24 @@ function Hero_Section() {
           <Image src="" width={64} height={64} alt="Avatar" className="bg-gray-500 rounded-full" />
         </div>
         <div className="text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl dark:text-slate-50">
             Creative Developer Based In Toronto.
           </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
+          <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-slate-100">
             Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
             fugiat veniam occaecat fugiat aliqua.
           </p>
           <div className="flex items-center justify-center mt-10 gap-x-6">
             <a
               href="#"
-              className="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+              className="rounded-md bg-primary-600  px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
             >
-              Get started
+              Get In Touch!
             </a>
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              Learn more <span aria-hidden="true">→</span>
-            </a>
+            <Link href="https://docs.google.com/document/d/13TFxDfDENVr2-qXO2_zdYv6jHs-m582U/edit?usp=sharing&ouid=114993777085049054802&rtpof=true&sd=true" className="inline-flex items-center gap-2 text-base font-semibold leading-6 text-gray-900 dark:text-primary-100">
+              <HiExternalLink className="mb-0.5 w-5 h-5" />
+              Resume
+            </Link>
           </div>
         </div>
       </div>
@@ -112,15 +116,16 @@ function Hero_Section() {
 function Project_Sample() {
   return (
     <>
-      <h2 className="text-4xl font-bold tracking-tight text-gray-900 capitalize">Signature Prjects I’ve made, over my carrer.</h2>
-      <p className="max-w-6xl mt-2">I’ve worked on tons of projects over the years but these are the ones that have stood out and I’m most proud of. Some are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved.</p>
+      <h2 className="text-4xl font-bold tracking-tight text-gray-900 capitalize dark:text-slate-50">Signature Prjects I’ve made, over my carrer.</h2>
+      <p className="max-w-6xl mt-2 dark:text-slate-50">I’ve worked on tons of projects over the years but these are the ones that have stood out and I’m most proud of. Some are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved.</p>
       <div className="grid grid-cols-1 my-8 lg:grid-cols-2">
         <Project_Card Headline="Paramount Properties LTD" Media="" Link="" />
-        <Project_Card Headline="Option One Staffing Agency" Media="" Link="" />
-        <Project_Card Headline="Believe Support Services" Media="" Link="" />
-        <Project_Card Headline="Marker App" Media="" Link="" />
-        <Project_Card Headline="Urban Greens Antigua" Media="" Link="" />
-        <Project_Card Headline="ParkSmart" Media="" Link="" />
+        <Project_Card Headline="Option One Staffing Agency" Media="" Link="https://optionone-staffing-agency.vercel.app/" />
+        <Project_Card Headline="Believe Support Services" Media="" Link="https://believe-support-services.vercel.app/" />
+        <Project_Card Headline="Marker App" Media="/brand/marker.svg" Link="" />
+        <Project_Card Headline="Urban Greens Antigua" Media="" Link="https://www.urbangreensantigua.com/" />
+        <Project_Card Headline="ParkSmart" Media="" Link="https://www.canva.com/design/DAEXeWFsbI4/GeiVkAU9MYeVHZc0SDYlJw/view?utm_content=DAEXeWFsbI4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" />
+        <Project_Card Headline="Antigua News Room Redesign" Media="" Link="https://www.figma.com/file/x5pcKa5FUxF85JMBolszC9/Antigua-News-Room?node-id=0%3A1&t=FmqPUlUteGYw2OQo-1" />
       </div>
     </>
   )
@@ -128,15 +133,15 @@ function Project_Sample() {
 function Contact_Form() {
   return (
     <>
-      <h3 className="text-4xl font-bold tracking-tight text-gray-900 capitalize">Get In Touch</h3>
-      <p className="mt-1 mb-8 leading-7 text-gray-600">
+      <h3 className="text-4xl font-bold tracking-tight text-gray-900 capitalize dark:text-slate-50">Get In Touch</h3>
+      <p className="mt-1 mb-8 leading-7 text-gray-600 dark:text-slate-50">
         Quam nunc nunc eu sed. Sed rhoncus quis ultricies ac pellentesque.
       </p>
       <form action="#" method="POST" className="">
         <div className="">
           <div className="flex flex-col gap-4 my-4">
             <div>
-              <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+              <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900 dark:text-slate-50">
                 First name
               </label>
               <div className="mt-2.5">
@@ -145,12 +150,12 @@ function Contact_Form() {
                   name="first-name"
                   id="first-name"
                   autoComplete="given-name"
-                  className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600"
+                  className="block w-full dark:bg-slate-900 rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 dark:text-slate-50 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600"
                 />
               </div>
             </div>
             <div>
-              <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+              <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900 dark:text-slate-50 dark:bg-slate-900">
                 Last name
               </label>
               <div className="mt-2.5">
@@ -159,12 +164,12 @@ function Contact_Form() {
                   name="last-name"
                   id="last-name"
                   autoComplete="family-name"
-                  className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600"
+                  className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 dark:text-slate-50  dark:bg-slate-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600"
                 />
               </div>
             </div>
             <div className="sm:col-span-2">
-              <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+              <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900 dark:text-slate-50 dark:bg-slate-900">
                 Email
               </label>
               <div className="mt-2.5">
@@ -173,12 +178,12 @@ function Contact_Form() {
                   name="email"
                   id="email"
                   autoComplete="email"
-                  className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600"
+                  className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 dark:text-slate-50  dark:bg-slate-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600"
                 />
               </div>
             </div>
             <div className="sm:col-span-2">
-              <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
+              <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900 dark:text-slate-50 dark:bg-slate-900">
                 Phone number
               </label>
               <div className="mt-2.5">
@@ -187,12 +192,12 @@ function Contact_Form() {
                   name="phone-number"
                   id="phone-number"
                   autoComplete="tel"
-                  className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600"
+                  className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 dark:text-slate-50  dark:bg-slate-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600"
                 />
               </div>
             </div>
             <div className="sm:col-span-2">
-              <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+              <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900 dark:text-slate-50 dark:bg-slate-900">
                 Message
               </label>
               <div className="mt-2.5">
@@ -200,7 +205,7 @@ function Contact_Form() {
                   name="message"
                   id="message"
                   rows={4}
-                  className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600"
+                  className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 dark:text-slate-50  dark:bg-slate-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600"
                   defaultValue={''}
                 />
               </div>
