a9babc89a8708177e8337b452ac4fa6bb6e97969
Justin Bento <bento.a.justin@gmail.com>
2023-02-24 20:24:49 -0500

    applied the animation updated the order loading time

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function index() {
       </Head>
       <Header />
       <div className="isolate">
-        <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+        <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem] order-3 animate-fade-in">
           <svg
             className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
             viewBox="0 0 1155 678"
@@ -42,9 +42,9 @@ export default function index() {
           </svg>
         </div>
         <main className="mb-32 space-y-32">
-          <section className="relative">
+          <section className="relative order-1 animate-fade-in">
             <Hero_Section />
-            <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+            <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]  order-4 animate-fade-in">
               <svg
                 className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
                 viewBox="0 0 1155 678"
