347c885bbabcedccdbebb50358ae4f6010c2da26
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-02-26 13:07:01 -0500

    included facebook meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,13 @@ export default function index() {
         <meta name="title" content="Justin Bento — Portfolio" />
         <meta name="keywords" content="Full-Stack, website design website developer" />
         <meta name="description" content="Explore my portfolio website showcasing my latest full-stack development work, featuring creative solutions for various clients and industries." />
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:image" content="/public/website-screenshot.webp" />
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://justin-bento.com/" />
+        <meta property="og:title" content="Justin Bento — Portfolio" />
+        <meta property="og:description" content="Explore my portfolio website showcasing my latest full-stack development work, featuring creative solutions for various clients and industries." />
         <meta property="og:image" content="/public/website-screenshot.webp" />
       </Head>
       <Header />
