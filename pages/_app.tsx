7a47eda352f687da20fa5a78ccf1c1ba11418b02
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-02-26 13:26:49 -0500

    removed vercel analytics from main project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   <>
     <Component {...pageProps} />
-    <Analytics />
   </>
 }
