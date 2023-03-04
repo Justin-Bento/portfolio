c400801b4cf9d1b94be0416a2d2630466ad27d02
Justin Bento <bento.a.justin@gmail.com>
2023-03-04 13:45:50 -0500

    mvoed contact section to it's own section

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,51 +1,150 @@
-import Head from "next/head";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { useForm, SubmitHandler } from "react-hook-form";
+import Head from "next/head";
 
-export default function uses() {
+type FormInputs = {
+  First_Name: string;
+  Last_Name: string;
+  Email: string;
+  Message: string;
+};
+export default function Contact() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>();
+  const onSubmit: SubmitHandler<FormInputs> = (data) => console.log(data);
+
   return (
     <>
       <Head>
-        {/* <!-- Primary Meta Tags --> */}
-        <title>Contact - Justin Bento Portfolio</title>
-        <meta name="title" content="Justin Bento Portfolio" />
-        <meta name="description" content="Join me, on this adventure through my portfolio. Experience my passion for design, dev, and music all located  in one convenient place." />
+        <title>Justin Bento — Contact</title>
+        <meta name="title" content="Justin Bento — Contact" />
+        <meta name="keywords" content="Full-Stack, website design website developer" />
+        <meta name="description" content="Explore my portfolio website showcasing my latest full-stack development work, featuring creative solutions for various clients and industries." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:image" content="/public/website-screenshot.webp" />
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://justin-bento.com/" />
+        <meta property="og:title" content="Justin Bento — Contact" />
+        <meta property="og:description" content="Explore my portfolio website showcasing my latest full-stack development work, featuring creative solutions for various clients and industries." />
+        <meta property="og:image" content="/public/website-screenshot.webp" />
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://justin-bento.com/" />
+        <meta property="twitter:title" content="Justin Bento — Contact" />
+        <meta property="twitter:description" content="Explore my portfolio website showcasing my latest full-stack development work, featuring creative solutions for various clients and industries." />
+        <meta property="twitter:image" content="/public/website-screenshot.webp" />
       </Head>
       <Header />
-      <main className="grid items-start p-4 my-16 lg:grid-cols-2 lg:items-center lg:my-24">
+      <main className="my-32 wrapper">
         <section className="">
-          <h1 className="text-5xl leading-relaxed capitalize lg:leading-[7rem] lg:text-8xl font-display text-[#443733]">Intrested in Collaberating Togther?</h1>
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 capitalize dark:text-slate-50">
+            Get In Touch
+          </h1>
+          <p className="mt-1 mb-8 leading-7 text-gray-600 dark:text-slate-50">
+            Quam nunc nunc eu sed. Sed rhoncus quis ultricies ac pellentesque.
+          </p>
         </section>
-        <Contact_Form />
+        <form onSubmit={handleSubmit(onSubmit)} method="POST" className="">
+          <div className="">
+            <div className="flex flex-col gap-4 my-4">
+              <div>
+                <label
+                  htmlFor="first-name"
+                  defaultValue="First_Name"
+                  className="block text-sm font-semibold leading-6 text-gray-900 dark:text-slate-50"
+                >
+                  First name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    {...register("First_Name")}
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="block w-full dark:bg-slate-900 rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 dark:text-slate-50 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="last-name"
+                  defaultValue="Last_Name"
+                  className="block text-sm font-semibold leading-6 text-gray-900 dark:text-slate-50 dark:bg-slate-900"
+                >
+                  Last name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    {...register("Last_Name")}
+                    id="last-name"
+                    autoComplete="family-name"
+                    className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 dark:text-slate-50  dark:bg-slate-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-semibold leading-6 text-gray-900 dark:text-slate-50 dark:bg-slate-900"
+                >
+                  Email
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="email"
+                    id="email"
+                    autoComplete="email"
+                    {...register("Email", { required: true })}
+                    className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 dark:text-slate-50  dark:bg-slate-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600"
+                  />
+                </div>
+                {errors.Email && (
+                  <span className="text-xs text-red-600">
+                    * Email field is required
+                  </span>
+                )}
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-semibold leading-6 text-gray-900 dark:text-slate-50 dark:bg-slate-900"
+                >
+                  Message
+                </label>
+                <div className="mt-2.5">
+                  <textarea
+                    id="message"
+                    rows={4}
+                    className="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 dark:text-slate-50  dark:bg-slate-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600"
+                    {...register("Message", { required: true })}
+                  />
+                  {errors.Message && (
+                    <span className="text-xs text-red-600">
+                      * Message field is required
+                    </span>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-end mt-8">
+              {/* errors will return when field validation fails  */}
+              <button
+                type="submit"
+                className="rounded-md bg-primary-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+              >
+                Send message
+              </button>
+            </div>
+          </div>
+        </form>
       </main>
       <Footer />
     </>
-  )
-}
-type Inputs = {
-  full_name: string,
-  subject: string,
-  email: string,
-  message: string,
-};
-
-function Contact_Form() {
-  const { register, handleSubmit,  formState: { errors } } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = data => `mailto:bento.a.justin@gmail.com?subject=${data.subject}&body=${data.full_name + data.message}`;
-  return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 max-w-prose" method="POST">
-      {/* register your input into the hook by invoking the "register" function */}
-      <input placeholder="Full Name" {...register("full_name")} className="block w-full p-2 border border-gray-600 rounded-md shadow-sm focus:border-gray-900 focus:ring-gray-900 sm:text-sm" />
-      <input placeholder="Subject" {...register("subject", { required: true })} className="block w-full p-2 border border-gray-600 rounded-md shadow-sm focus:border-gray-900 focus:ring-gray-900 sm:text-sm" />
-      
-      {/* include validation with required or other standard HTML validation rules */}
-      <input placeholder="jane.doe@email.com" {...register("email", { required: true })} className="block w-full p-2 border border-gray-600 rounded-md shadow-sm focus:border-gray-900 focus:ring-gray-900 sm:text-sm" />
-      <input placeholder="Write something longer than this.." {...register("message", { required: true })} className="block w-full p-2 border border-gray-600 rounded-md shadow-sm focus:border-gray-900 focus:ring-gray-900 sm:text-sm" />
-      {/* errors will return when field validation fails  */}
-      {errors.email && <span>This field is required</span>}
-      <input type="submit" className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-[#739884] border border-transparent rounded-md shadow-sm hover:bg-[#475E52] focus:outline-none focus:ring-2 focus:ring-[#475E52] focus:ring-offset-2 hover:cursor-pointer" />
-    </form>
   );
 }
