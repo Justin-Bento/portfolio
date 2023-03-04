d59f734b284a5a5620cbac4d9f5f9b122cfe4f78
Justin Bento <bento.a.justin@gmail.com>
2023-03-04 13:47:28 -0500

    moved project section to their own page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,10 +1,31 @@
 import Project_Card from "@/components/Project_Card";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 export default function Projects() {
   return (
     <>
+      <Head>
+        <title>Justin Bento — Projects</title>
+        <meta name="title" content="Justin Bento — Projects" />
+        <meta name="keywords" content="Full-Stack, website design website developer" />
+        <meta name="description" content="Explore my portfolio website showcasing my latest full-stack development work, featuring creative solutions for various clients and industries." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:image" content="/public/website-screenshot.webp" />
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://justin-bento.com/" />
+        <meta property="og:title" content="Justin Bento — Projects" />
+        <meta property="og:description" content="Explore my portfolio website showcasing my latest full-stack development work, featuring creative solutions for various clients and industries." />
+        <meta property="og:image" content="/public/website-screenshot.webp" />
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://justin-bento.com/" />
+        <meta property="twitter:title" content="Justin Bento — Projects" />
+        <meta property="twitter:description" content="Explore my portfolio website showcasing my latest full-stack development work, featuring creative solutions for various clients and industries." />
+        <meta property="twitter:image" content="/public/website-screenshot.webp" />
+      </Head>
       <Header />
       <main className="my-32 space-y-8">
         <section className="wrapper">
