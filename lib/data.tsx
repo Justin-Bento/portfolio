053af79ea282753ca4e70f9745cca08f77d5e270
Justin Bento <bento.a.justin@gmail.com>
2023-03-05 20:03:52 -0500

    moved sample data to it;s own project.

---
+++ lib/data.tsx
@@ -0,0 +1,9 @@
+export const projects = [
+  { id: 1, title: "Paramount Properties LTD", body: "", image: "/projects/paramount-properties-ltd.png", url: "https://www.figma.com/file/x5pcKa5FUxF85JMBolszC9/Antigua-News-Room?node-id=0%3A1&t=FmqPUlUteGYw2OQo-1" },
+  { id: 1, title: "Option One Staffing Agency", body: "", image: "/projects/option-one-staffing-agency.webp", url: "https://optionone-staffing-agency.vercel.app/" },
+  { id: 1, title: "Believe Support Services", body: "", image: "/projects/believe-support-services-logo.webp", url: "https://believe-support-services.vercel.app/" },
+  { id: 1, title: "Marker App", body: "", image: "/projects/marker-brand.png", url: "https://www.figma.com/file/x5pcKa5FUxF85JMBolszC9/Antigua-News-Room?node-id=0%3A1&t=FmqPUlUteGYw2OQo-1" },
+  { id: 1, title: "Urban Greens Antigua", body: "", image: "/projects/urban-greens-antigua.png", url: "https://www.urbangreensantigua.com/" },
+  { id: 1, title: "Antigua News Room Redesign", body: "", image: "/projects/antigua-news-room-logo.png", url: "https://www.figma.com/file/x5pcKa5FUxF85JMBolszC9/Antigua-News-Room?node-id=0%3A1&t=FmqPUlUteGYw2OQo-1" },
+  { id: 1, title: "ParkSmart", body: "", image: "/projects/parksmart-project.png", url: "https://www.canva.com/design/DAEXeWFsbI4/GeiVkAU9MYeVHZc0SDYlJw/view?utm_content=DAEXeWFsbI4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" },
+]
