2773f039ccf41284b89676120f2dbbd8aa56f0bf
Justin Bento <bento.a.justin@gmail.com>
2023-03-15 15:27:30 -0400

    created a data set for social media

--- lib/data.tsx
+++ lib/data.tsx
@@ -7,3 +7,11 @@ export const projects = [
   { id: 1, title: "Antigua News Room Redesign", body: "", image: "/projects/antigua-news-room-logo.png", url: "https://www.figma.com/file/x5pcKa5FUxF85JMBolszC9/Antigua-News-Room?node-id=0%3A1&t=FmqPUlUteGYw2OQo-1" },
   { id: 1, title: "ParkSmart", body: "", image: "/projects/parksmart-project.png", url: "https://www.canva.com/design/DAEXeWFsbI4/GeiVkAU9MYeVHZc0SDYlJw/view?utm_content=DAEXeWFsbI4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" },
 ]
+
+export const social_media = [
+  { id: 1, title: "Follow Me On Twitter" },
+  { id: 2, title: "Follow Me On LinkedIn" },
+  { id: 3, title: "Follow Me On GitHub" },
+  { id: 4, title: "Follow Me On SoundCloud" },
+]
+// More items...
