e99027b15ad578c4ecdc8adb3ca90df51440da90
Justin Bento <bento.a.justin@gmail.com>
2023-03-16 01:18:50 -0400

    formatted document with prettier

--- lib/data.tsx
+++ lib/data.tsx
@@ -1,17 +1,68 @@
 export const projects = [
-  { id: 1, title: "Paramount Properties LTD", body: "", image: "/projects/paramount-properties-ltd.png", url: "https://www.figma.com/file/x5pcKa5FUxF85JMBolszC9/Antigua-News-Room?node-id=0%3A1&t=FmqPUlUteGYw2OQo-1" },
-  { id: 1, title: "Option One Staffing Agency", body: "", image: "/projects/option-one-staffing-agency.webp", url: "https://optionone-staffing-agency.vercel.app/" },
-  { id: 1, title: "Believe Support Services", body: "", image: "/projects/believe-support-services-logo.webp", url: "https://believe-support-services.vercel.app/" },
-  { id: 1, title: "Marker App", body: "", image: "/projects/marker-brand.png", url: "https://www.figma.com/file/x5pcKa5FUxF85JMBolszC9/Antigua-News-Room?node-id=0%3A1&t=FmqPUlUteGYw2OQo-1" },
-  { id: 1, title: "Urban Greens Antigua", body: "", image: "/projects/urban-greens-antigua.png", url: "https://www.urbangreensantigua.com/" },
-  { id: 1, title: "Antigua News Room Redesign", body: "", image: "/projects/antigua-news-room-logo.png", url: "https://www.figma.com/file/x5pcKa5FUxF85JMBolszC9/Antigua-News-Room?node-id=0%3A1&t=FmqPUlUteGYw2OQo-1" },
-  { id: 1, title: "ParkSmart", body: "", image: "/projects/parksmart-project.png", url: "https://www.canva.com/design/DAEXeWFsbI4/GeiVkAU9MYeVHZc0SDYlJw/view?utm_content=DAEXeWFsbI4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" },
-]
+  {
+    id: 1,
+    title: "Paramount Properties LTD",
+    body: "",
+    image: "/projects/paramount-properties-ltd.png",
+    url: "https://www.figma.com/file/x5pcKa5FUxF85JMBolszC9/Antigua-News-Room?node-id=0%3A1&t=FmqPUlUteGYw2OQo-1",
+  },
+  {
+    id: 1,
+    title: "Option One Staffing Agency",
+    body: "",
+    image: "/projects/option-one-staffing-agency.webp",
+    url: "https://optionone-staffing-agency.vercel.app/",
+  },
+  {
+    id: 1,
+    title: "Believe Support Services",
+    body: "",
+    image: "/projects/believe-support-services-logo.webp",
+    url: "https://believe-support-services.vercel.app/",
+  },
+  {
+    id: 1,
+    title: "Marker App",
+    body: "",
+    image: "/projects/marker-brand.png",
+    url: "https://www.figma.com/file/x5pcKa5FUxF85JMBolszC9/Antigua-News-Room?node-id=0%3A1&t=FmqPUlUteGYw2OQo-1",
+  },
+  {
+    id: 1,
+    title: "Urban Greens Antigua",
+    body: "",
+    image: "/projects/urban-greens-antigua.png",
+    url: "https://www.urbangreensantigua.com/",
+  },
+  {
+    id: 1,
+    title: "Antigua News Room Redesign",
+    body: "",
+    image: "/projects/antigua-news-room-logo.png",
+    url: "https://www.figma.com/file/x5pcKa5FUxF85JMBolszC9/Antigua-News-Room?node-id=0%3A1&t=FmqPUlUteGYw2OQo-1",
+  },
+  {
+    id: 1,
+    title: "ParkSmart",
+    body: "",
+    image: "/projects/parksmart-project.png",
+    url: "https://www.canva.com/design/DAEXeWFsbI4/GeiVkAU9MYeVHZc0SDYlJw/view?utm_content=DAEXeWFsbI4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink",
+  },
+];
 
-export const social_media = [
-  { id: 1, title: "Follow Me On Twitter" },
-  { id: 2, title: "Follow Me On LinkedIn" },
-  { id: 3, title: "Follow Me On GitHub" },
-  { id: 4, title: "Follow Me On SoundCloud" },
-]
-// More items...
+export const social = [
+  {
+    name: 'Facebook',
+    title: "Follow Me On facebook",
+    href: '#',
+    icon: (props: any) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+];
