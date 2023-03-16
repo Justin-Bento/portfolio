8c201bebd81fc310123cdd4eb3872927c0200e5a
Justin Bento <bento.a.justin@gmail.com>
2023-03-16 01:20:29 -0400

    updated call to actions.

--- pages/index.tsx
+++ pages/index.tsx
@@ -108,15 +108,14 @@ function Hero_Section() {
             If you&#39;re interested in collaborating with me on a project or if there&#39;s anything missing, please don&#39;t hesitate to reach out. 
           </p>
           <div className="flex items-center justify-center mt-10 gap-x-6">
-            <a
-              href="#"
+            <Link
+              href="/projects"
               className="rounded-md bg-primary-600  px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
             >
-              Get In Touch!
-            </a>
-            <Link href="https://docs.google.com/document/d/13TFxDfDENVr2-qXO2_zdYv6jHs-m582U/edit?usp=sharing&ouid=114993777085049054802&rtpof=true&sd=true" className="inline-flex items-center gap-2 text-base font-semibold leading-6 text-gray-900 dark:text-primary-100">
-              <HiExternalLink className="mb-0.5 w-5 h-5" />
-              Resume
+              View Projects
+            </Link>
+            <Link href="/about" className="inline-flex items-center gap-2 text-base font-semibold leading-6 text-gray-900 dark:text-primary-100">
+              Get To Know Me
             </Link>
           </div>
         </div>
