8eed603e46db1a5e61994bb3c4159392f7222842
Justin Bento <bento.a.justin@gmail.com>
2023-03-26 18:09:09 -0400

    updated container issues for mobile design

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -33,7 +33,7 @@ export default function Projects() {
           <h2 className="text-4xl font-bold tracking-tight text-gray-900 capitalize dark:text-slate-50">Signature Projects I’ve made.</h2>
           <p className="mt-2 text-base leading-8 text-gray-600 dark:text-slate-100">I’ve worked on tons of projects over the years but these are the ones that have stood out and I’m most proud of. Some are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved.</p>
         </section>
-        <section className="grid grid-cols-1 my-8 wrapper lg:grid-cols-2">
+        <section className="grid grid-cols-1 mx-auto my-8 lg:grid-cols-2 max-w-7xl">
           {projects.map((data) => {
             return <Project_Card key={data.title.toString().toLowerCase()} Headline={data.title} Supporting={data.body} Media={data.image!} Link={data.url} />
           })}
