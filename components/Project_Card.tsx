df9c5ba7cccaa2a2eb9b898d0fd9a5db38f57fe9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-04-30 10:26:28 +0100

    fixed typography for projetc card

--- components/Project_Card.tsx
+++ components/Project_Card.tsx
@@ -20,9 +20,9 @@ export default function Project_Card(props: Project_Card_Props) {
               </div>
             </div>
             <div className="px-4 pb-4 space-y-2">
-              <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">{props.Headline}</h2>
-              <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-300">{props.Supporting || "Creating technology to empower civilians to explore space on their own terms."}</p>
-              <button type="button" className="inline-flex items-center gap-2 py-2 mt-4 text-xs font-medium text-gray-700 dark:text-zinc-100">
+              <h2 className="mt-6 title-medium">{props.Headline}</h2>
+              <p className="mt-2 body-medium">{props.Supporting || "Creating technology to empower civilians to explore space on their own terms."}</p>
+              <button type="button" className="inline-flex items-center gap-2 py-2 mt-4 text-gray-700 body-small dark:text-zinc-100">
                 <Link_Icon />
                 View Project
               </button>
@@ -36,7 +36,7 @@ export default function Project_Card(props: Project_Card_Props) {
 
 function Link_Icon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mb-0.5">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3">
       <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
     </svg>
   )
