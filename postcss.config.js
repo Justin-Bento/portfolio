c0a8376b9722993fb00d71454815f84d004dfba2
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-04 23:42:35 -0400

    removed postcss conent

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: {
-      content: [
-        './pages/**/*.{js,jsx,ts,tsx}',
-        './components/**/*.{js,jsx,ts,tsx}',
-      ],
-    },
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
