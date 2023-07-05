5f8ab79409f02235c10608a8225b0b7fafc4ad83
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-05 15:38:32 -0400

    only allow images from sanity and unsplash cdn

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  image: {}
+  image: {
+    domains: [
+      "cdn.sanity.io",
+      "images.unsplash.com",
+      // ...
+    ]
+  }
 }
 
 module.exports = nextConfig
