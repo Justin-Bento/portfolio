81e2aba614f4322a925292e310ced92d56b7349b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-05 16:39:05 -0400

    fixed file and removed unused packages

--- portfolio-studio/sanity.cli.ts
+++ portfolio-studio/sanity.cli.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from 'sanity'
-import { deskTool } from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { schemaTypes } from './schemas'
 
 export default defineConfig({
   name: 'default',
   title: 'portfolio-studio',
-
   projectId: 'v30obm3j',
   dataset: 'production',
-
-  plugins: [ deskTool(), 
-    visionTool()
-  ],
-
   schema: {
     types: schemaTypes,
   },
