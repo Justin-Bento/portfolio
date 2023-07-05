ac1e4c114075aab80339507ca499d6530e31736b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-05 18:16:40 -0400

    fixed up imports

--- portfolio-studio/sanity.config.ts
+++ portfolio-studio/sanity.config.ts
@@ -1,18 +1,21 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import {visionTool} from '@sanity/vision';
+import {defineConfig} from 'sanity';
+import {deskTool} from 'sanity/desk';
+import {schemaTypes} from './schemas';
 
 export default defineConfig({
+  basePath: '/studio',
   name: 'default',
   title: 'portfolio-studio',
-
   projectId: 'v30obm3j',
   dataset: 'production',
-  basePath: '/studio',
-
-  plugins: [deskTool(), visionTool()],
-
+  // ...
+  plugins: [
+    deskTool(),
+    visionTool(),
+    // ...
+  ],
+  // ...
   schema: {
     types: schemaTypes,
   },
