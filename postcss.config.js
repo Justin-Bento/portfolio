d22c539c8767f32488302fad7d7a04a0a2f6071e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-04 23:18:31 -0400

    Only add tailwindcss styles to these folders

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+      content: [
+        './pages/**/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,jsx,ts,tsx}',
+      ],
+    },
     autoprefixer: {},
   },
 }
