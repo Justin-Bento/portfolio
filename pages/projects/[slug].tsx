e0af77b187f7c2e64e7cb24053b2d4ed1da6d369
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-07 23:58:51 -0400

    included metat-data for the article

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -1,11 +1,31 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { client } from "@/sanityClient";
+import Head from "next/head";
 
 export default function ProjectsSlug({post}: any) {
   console.log(post)
   return (
     <>
+      <Head>
+        <title>Justin Bento — {post.title}</title>
+        <meta name="keywords" content="Full-Stack, website design website developer" />
+        <meta name="description" content={`${post.description}`} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:image" content="/public/website-screenshot.webp" />
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://justin-bento.com/" />
+        <meta property="og:title" content={`Justin Bento — ${post.title}`}/>
+        <meta property="og:description" content={`${post.description}`} />
+        <meta property="og:image" content="/public/website-screenshot.webp" />
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://justin-bento.com/" />
+        <meta property="twitter:title" content={`Justin Bento — ${post.title}`}/>
+        <meta property="twitter:description" content={`${post.description}`} />
+        <meta property="twitter:image" content="/public/website-screenshot.webp" />
+      </Head>
       <Header />
       <main className="p-1 my-12 md:my-24">
         <div className="lg:flex lg:items-start">
