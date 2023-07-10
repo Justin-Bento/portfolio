aea3a4dd6f868e4d4521f1a24dd7dde86c009a9f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-10 03:31:48 -0400

    removed tailwind css classes into a css module

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Footer from "@/components/Footer";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import home from "../styles/Home.module.css"
 
 
 export default function index() {
@@ -29,10 +30,10 @@ export default function index() {
         <meta property="twitter:image" content="/public/website-screenshot.webp" />
       </Head>
       <Header />
-      <div className="isolate">
-        <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem] order-3 animate-fade-in">
+      <div className={home.isolate}>
+        <div className={home.bgPattern1}>
           <svg
-            className="relative left-[calc(50%-11rem)] -z-10 max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] h-screen"
+            className={home.bgPattern2}
             viewBox="0 0 1155 678"
           >
             <path
@@ -57,10 +58,36 @@ export default function index() {
         </div>
         <main className="p-4 py-4 space-y-32 md:my-16">
           <section className="relative order-1 animate-fade-in">
-            <Hero_Section />
-            <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]  order-4 animate-fade-in">
+            <div className={home.hero}>
+              <div className={home.mainImage}>
+              <Image src="/media/jusitn-bento--linkin.png" fill quality={80} alt="Avatar" className={home.mainImageLin} />
+              
+              </div>
+              <div>
+                <p>Hey i&#39;m Justin Bento.</p>
+                <h1 className={home.headline}>
+                  A Creative Developer Based In Toronto.
+                </h1>
+                <p className={home.body}>
+                  Here, all the information about me, my work, and my accomplishments gathered in one place.
+                  If you&#39;re interested in collaborating with me on a project or if there&#39;s anything missing, please don&#39;t hesitate to reach out.
+                </p>
+                <div className={home.hero_actions}>
+                  <Link
+                    href="/projects"
+                    className={home.btnPrimary}
+                  >
+                    View Projects
+                  </Link>
+                  <Link href="/about" className={home.btnTransparent}>
+                    Get To Know Me
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className={home.bgPattern3}>
               <svg
-                className="relative left-[calc(50%+3rem)] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] h-screen"
+                className={home.bgPattern4}
                 viewBox="0 0 1155 678"
               >
                 <path
@@ -85,37 +112,6 @@ export default function index() {
             </div>
           </section>
         </main>
-      </div>
-      <Footer />
-    </>
-  )
-}
-function Hero_Section() {
-  return (
-    <>
-      <div className="flex flex-col items-start justify-start mx-auto prose md:prose-lg md:items-center md:justify-center text-start md:text-center place-items-center place-content-center dark:prose-invert">
-        <Image src="/media/jusitn-bento--linkin.png" width={72} height={72} alt="Avatar" className="bg-gray-500 rounded-full" />
-        <div>
-          <p>Hey i&#39;m Justin Bento.</p>
-          <h1>
-            A Creative Developer Based In Toronto.
-          </h1>
-          <p>
-            Here, all the information about me, my work, and my accomplishments gathered in one place. 
-            If you&#39;re interested in collaborating with me on a project or if there&#39;s anything missing, please don&#39;t hesitate to reach out. 
-          </p>
-          <div className="flex flex-col items-center justify-center gap-4 mt-8 md:flex-row md:max-w-xs md:mx-auto">
-            <Link
-              href="/projects"
-              className="w-full decoration-transparent border border-primary-600 rounded-md bg-primary-600  px-3.5 py-2.5 body-medium font-medium text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
-            >
-              View Projects
-            </Link>
-            <Link href="/about" className="w-full decoration-transparent rounded-md bg-primary-600/0  px-3.5 py-2.5 body-medium font-medium border transition-all hover:bg-primary-500/10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600">
-              Get To Know Me
-            </Link>
-          </div>
-        </div>
       </div>
     </>
   )
