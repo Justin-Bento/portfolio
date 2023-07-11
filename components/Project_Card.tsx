2e78bd1997b09546c2820e73e64c10438d094ea5
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-11 17:21:52 -0400

    replaced svg with react-icons

--- components/Project_Card.tsx
+++ components/Project_Card.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import project_card from "../styles/project_card.module.css"
+import { RiLinksFill } from "react-icons/ri";
 interface Project_Card_Props {
   Link: string,
   Headline: string,
@@ -22,9 +23,7 @@ export default function Project_Card(props: Project_Card_Props) {
             <h2 className={project_card.title}>{props.Headline}</h2>
             <p className={project_card.supporting}>{props.Supporting || "Creating technology to empower civilians to explore space on their own terms."}</p>
             <button type="button" className={project_card.actions}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={project_card.icons}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
-              </svg>
+              <RiLinksFill className={project_card.icons} />
               View Project
             </button>
           </div>
