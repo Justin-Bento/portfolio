2d13f61f9d27c9fa97660c342df6326054e837b6
Justin Bento <bento.a.justin@gmail.com>
2023-07-12 14:47:18 -0400

    updated image, list, and block

--- components/RichTextComponents.tsx
+++ components/RichTextComponents.tsx
@@ -1,32 +1,47 @@
 import Image from "next/image";
 import { urlFor } from "@/sanityClient";
-import articles from "@/styles/articles.module.css";
+import articles from '@/styles/articles.module.css';
 
-const RichTextComponents =  {
+const RichTextComponents = {
   types: {
     image: ({ value }: any) => {
       return (
         <>
-          <div className={`${articles.imageContainer}`}>
+          <div className={`${articles.articleImageContainer}`}>
             <Image
               fill
               src={urlFor(value).url()}
               quality={40}
               alt=""
               loading="lazy"
-              className={`${articles.imageMedia}`}
             />
           </div>
         </>
       );
     },
   },
+  list: {
+    bullet: ({ children }: any) => {
+      return <ul>{children}</ul>;
+    },
+    number: ({ children }: any) => {
+      return <ol>{children}</ol>;
+    },
+  },
   block: {
     // Ex. 1: customizing common block types
-    h1: ({ children }: any) => <h1 className={articles.headline1}>{children}</h1>,
-    h2: ({ children }: any) => <h2 className={articles.headline2}>{children}</h2>,
-    h3: ({ children }: any) => <h3 className={articles.headline3}>{children}</h3>,
-    h4: ({ children }: any) => <h4 className={articles.headline4}>{children}</h4>,
+    h1: ({ children }: any) => {
+      return <h1 className={articles.headline1}>{children}</h1>;
+    },
+    h2: ({ children }: any) => {
+      return <h2 className={articles.headline2}>{children}</h2>;
+    },
+    h3: ({ children }: any) => {
+      return <h3 className={articles.headline3}>{children}</h3>;
+    },
+    h4: ({ children }: any) => {
+      return <h4 className={articles.headline4}>{children}</h4>;
+    },
   },
 };
 
