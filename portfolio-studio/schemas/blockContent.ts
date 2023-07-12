0fe2d8baa658af953e8668f55917bab18221692b
Justin Bento <bento.a.justin@gmail.com>
2023-07-12 14:47:25 -0400

    updated image, list, and block

--- portfolio-studio/schemas/blockContent.ts
+++ portfolio-studio/schemas/blockContent.ts
@@ -24,10 +24,11 @@ export default defineType({
       // use your content.
       styles: [
         {title: 'Normal', value: 'normal'},
-        {title: 'H1', value: 'h1'},
         {title: 'H2', value: 'h2'},
         {title: 'H3', value: 'h3'},
         {title: 'H4', value: 'h4'},
+        {title: 'H5', value: 'h5'},
+        {title: 'H6', value: 'h5'},
         {title: 'Quote', value: 'blockquote'},
       ],
       lists: [{title: 'Bullet', value: 'bullet'}],
