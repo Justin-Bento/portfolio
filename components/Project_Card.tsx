65fc830f95a011de67a3ce8bb2f8dc00eb8b9a74
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-14 12:37:11 -0400

    changed classNames to be descriptive

--- components/Project_Card.tsx
+++ components/Project_Card.tsx
@@ -12,17 +12,17 @@ interface Project_Card_Props {
 export default function Project_Card(props: Project_Card_Props) {
   return (
     <>
-      <Link href={props.Link} className={style.project_card_links}>
+      <Link href={props.Link} className={style.project_card}>
         <article className={style.project_card_surface}>
-          <div className={style.project_card_iconSurface}>
-            <div className={style.project_card_iconContainer}>
+          <div className={style.project_card_icon}>
+            <div className={style.project_card_icon_box}>
               <Image width={40} height={40} quality={40} src={props.Media} className={style.project_card_media} alt={props.Headline} />
             </div>
           </div>
           <div className="">
             <h2 className={style.project_card_title}>{props.Headline}</h2>
             <p className={style.project_card_supporting}>{props.Supporting || "Creating technology to empower civilians to explore space on their own terms."}</p>
-            <button type="button" className={style.project_card_actions}>
+            <button type="button" className={style.project_card_footer}>
               <RiLinksFill className={style.project_card_icons} />
               View Project
             </button>
