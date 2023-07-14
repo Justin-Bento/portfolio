365ffbb9462a9451044b0be6982823cb205362a7
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-14 12:35:01 -0400

    changed import name to style and named classes with project_card_className

--- components/Project_Card.tsx
+++ components/Project_Card.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import project_card from "../styles/project_card.module.css"
+import style from "../styles/project_card.module.css"
 import { RiLinksFill } from "react-icons/ri";
 interface Project_Card_Props {
   Link: string,
@@ -12,18 +12,18 @@ interface Project_Card_Props {
 export default function Project_Card(props: Project_Card_Props) {
   return (
     <>
-      <Link href={props.Link} className={project_card.links}>
-        <article className={project_card.surface}>
-          <div className={project_card.iconSurface}>
-            <div className={project_card.iconContainer}>
-              <Image width={40} height={40} quality={40} src={props.Media} className={project_card.media} alt={props.Headline} />
+      <Link href={props.Link} className={style.project_card_links}>
+        <article className={style.project_card_surface}>
+          <div className={style.project_card_iconSurface}>
+            <div className={style.project_card_iconContainer}>
+              <Image width={40} height={40} quality={40} src={props.Media} className={style.project_card_media} alt={props.Headline} />
             </div>
           </div>
           <div className="">
-            <h2 className={project_card.title}>{props.Headline}</h2>
-            <p className={project_card.supporting}>{props.Supporting || "Creating technology to empower civilians to explore space on their own terms."}</p>
-            <button type="button" className={project_card.actions}>
-              <RiLinksFill className={project_card.icons} />
+            <h2 className={style.project_card_title}>{props.Headline}</h2>
+            <p className={style.project_card_supporting}>{props.Supporting || "Creating technology to empower civilians to explore space on their own terms."}</p>
+            <button type="button" className={style.project_card_actions}>
+              <RiLinksFill className={style.project_card_icons} />
               View Project
             </button>
           </div>
