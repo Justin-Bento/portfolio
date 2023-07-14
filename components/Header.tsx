91a702060325ce5609a6161cc43b2fa6aec4938e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-14 12:02:18 -0400

    created a section box_2

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,18 +17,20 @@ export default function Appbar() {
             </Link>
             <RiMenuFill className={appbar.navbar_icons} onClick={onClick} />
           </section>
-                <nav>
-                  <ul className={appbar.navbar_list}>
-                    <Link href="/" className={appbar.navbar_list_item}>Home</Link>
-                    <Link href="/about" className={appbar.navbar_list_item}>About</Link>
-                    <Link href="/projects" className={appbar.navbar_list_item}>Projects</Link>
-                    <Link href="/contact" className={appbar.navbar_list_item}>Contact</Link>
-                  </ul>
-                </nav>
-                <section className={appbar.navbar_box_3}>
-                  <RiExternalLinkFill />
-                  <Link href="https://www.linkedin.com/in/justin--bento/" target="_blank" className={appbar.navbar_social_link}>LinkedIn</Link>
-                </section>
+          <section className="">
+            <nav>
+              <ul className={appbar.navbar_list}>
+                <Link href="/" className={appbar.navbar_list_item}>Home</Link>
+                <Link href="/about" className={appbar.navbar_list_item}>About</Link>
+                <Link href="/projects" className={appbar.navbar_list_item}>Projects</Link>
+                <Link href="/contact" className={appbar.navbar_list_item}>Contact</Link>
+              </ul>
+            </nav>
+          </section>
+          <section className={appbar.navbar_box_3}>
+            <RiExternalLinkFill />
+            <Link href="https://www.linkedin.com/in/justin--bento/" target="_blank" className={appbar.navbar_social_link}>LinkedIn</Link>
+          </section>
         </div>
       </header>
     </>
