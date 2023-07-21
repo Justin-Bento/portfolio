ef0b0a58fa81e2053ecee1539df16981306b35c2
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-07-20 21:15:20 -0400

    aded code block to sanity block content

--- portfolio-studio/schemas/blockContent.ts
+++ portfolio-studio/schemas/blockContent.ts
@@ -64,5 +64,9 @@ export default defineType({
       type: 'image',
       options: {hotspot: true},
     }),
+    defineArrayMember({
+      type: 'code',
+      title: 'Code block',
+    }),
   ],
 })
