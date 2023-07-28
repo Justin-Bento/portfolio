2889496e220d8a1b80748e0a6eddec9b21c9346e
Justin Bento <bento.a.justin@gmail.com>
2023-07-28 16:09:26 -0400

    replaced css styles with tailwind

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -24,7 +24,7 @@ export default function ContactForm() {
       </section>
       <form onSubmit={handleSubmit(sendToFormspree)} method="POST" className="">
         <div className="">
-          <div className={contact.layout}>
+          <div className="flex h-10 w-full rounded-md border border-input bg-white/10 px-3 py-2 text-sm ring-offset-slate-200 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50">
             <div>
               <label
                 htmlFor="first-name"
