85f2d29c7b04d50d37d573692d6298733d1a938d
Justin Bento <bento.a.justin@gmail.com>
2023-07-28 15:20:51 -0400

    fixed styling for html website

--- components/RichTextComponents.tsx
+++ components/RichTextComponents.tsx
@@ -6,13 +6,14 @@ const RichTextComponents = {
     image: ({ value }: any) => {
       return (
         <>
-          <div className={`${style.blog_image_container}`}>
+          <div className="relative w-full aspect-video mb-4">
             <Image
               fill
               src={urlFor(value).url()}
               quality={40}
               alt=""
               loading="lazy"
+              className="block object-fit object-center rounded-xl"
             />
           </div>
         </>
@@ -30,17 +31,21 @@ const RichTextComponents = {
   block: {
     // Ex. 1: customizing common block types
     h1: ({ children }: any) => {
-      return <h1 className={`${style.headline1cs}`}>{children}</h1>;
+      return <h1 className="">{children}</h1>;
     },
     h2: ({ children }: any) => {
-      return <h2 className={style.headline2}>{children}</h2>;
+      return <h2 className="scroll-m-20 border-b pb-2 text-3xl font-extrabold tracking-wide transition-colors first:mt-0">{children}</h2>;
     },
     h3: ({ children }: any) => {
-      return <h3 className={style.headline3}>{children}</h3>;
+      return <h3 className="scroll-m-20 text-2xl font-bold tracking-normal">{children}</h3>;
     },
     h4: ({ children }: any) => {
-      return <h4 className={style.headline4}>{children}</h4>;
+      return <h4 className="scroll-m-20 text-xl font-semibold tracking-wide block">{children}</h4>;
     },
+    p: ({ children }: any) => {
+      return <p className="">{children}</p>;
+    },
+    
   },
 };
 
