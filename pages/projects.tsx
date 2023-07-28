d8287bd75dd66a196156faacdae06b83d8e5f3d9
Justin Bento <bento.a.justin@gmail.com>
2023-07-28 14:38:35 -0400

    formatted project page with tailwind code

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,12 +1,52 @@
+import { Button } from '@/components/Button';
 import { client, urlFor } from '../sanityClient';
 import { AllPosts } from '../typings';
 import { GetStaticProps } from "next";
 
 import React from 'react'
+import { RiCodeBoxLine, RiComputerLine, RiLink } from 'react-icons/ri';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function Projects() {
+export default function Projects({ posts }: any) {
   return (
-    <div>projects</div>
+    <div className="lg:container lg:mx-auto lg:my-32">
+      <span className="">
+        <h1 className="block tracking-normal [text-wrap:balance] text-4xl font-extrabold sm:text-5xl text-neutral-950">A Showcase of Creativity</h1>
+        <p className="mt-4 tracking-wide text-xl text-neutral-600 max-w-[90ch]">Welcome to my project gallery! Here, you&#39;ll find a collection of my diverse creations, ranging from interactive multimedia to visually captivating designs. Take a moment to browse through my work and experience the art of storytelling through technology</p>
+      </span>
+      <div className="grid grid-cols-1 gap-8 mt-8 lg:grid-cols-2">
+        {posts.map((post: any) => {
+          return (
+            <>
+              <div className="overflow-hidden rounded-lg bg-white shadow ring-2 ring-gray-300">
+                <div className="px-4 py-5 sm:p-8">
+                  <span className="mb-6 shadow  relative flex h-8 w-8 shrink-0 overflow-hidden rounded-full">
+                    <Image fill src={urlFor(post.mainImage).url()} alt={`Image of ${post.title.toLowerCase()}`} />
+                  </span>
+                  <h2 className="font-display text-2xl font-semibold text-neutral-950">{post.title}</h2>
+                  <p className=" my-2 text-base text-neutral-600 tracking-wide line-clamp-2">{post.description}</p>
+                  <div className="flex items-center gap-4">
+                    <Link href={`/projects/${post.slug.current}`} >
+                      <Button variant="outline" size="sm" className='mt-4 flex items-center gap-1'>
+                        <RiComputerLine />
+                        View Project
+                      </Button>
+                    </Link>
+                    <Link href={post.liveDemo} target="_blank">
+                      <Button variant="outline" size="sm" className='mt-4 flex items-center gap-1'>
+                        <RiCodeBoxLine />
+                        View Code
+                      </Button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
@@ -16,6 +56,8 @@ export const getStaticProps: GetStaticProps<AllPosts> = async () => {
       _id, 
       title, 
       description, 
+      projectCode,
+    liveDemo,
       slug,   
       mainImage
     }
