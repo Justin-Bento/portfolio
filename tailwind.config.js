1b115432fed71caa73f999699b938ae6473b9746
Justin Bento <bento.a.justin@gmail.com>
2023-07-28 14:04:02 -0400

    extended sans font-family to use Satoshi

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
     //...
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Satoshi", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
