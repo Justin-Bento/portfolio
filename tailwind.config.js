1c9d25e54a9d428e792a93ba651cdfb71b4d51cf
Justin Bento <bento.a.justin@gmail.com>
2023-07-28 18:47:42 -0400

    added just in time compiler

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
