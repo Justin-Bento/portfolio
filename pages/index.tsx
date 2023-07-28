271fa0b8d08f69ace280a9be21f859e4f1694875
Justin Bento <bento.a.justin@gmail.com>
2023-07-28 19:10:20 -0400

    added background pattern

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ export default function index() {
         <meta name="title" content="Justin Bento  - Portfolio" />
         <meta name="description" content="Join me on an journey from multimedia design to web development. Explore interactive digital media with a passion for coding. " />
       </Head>
+      <BackgroundPattern />
       <div aria-label="home-page" className="p-4 lg:p-0 lg:container h-screen grid place-content-center lg:text-center [text-wrap:balance] mx-auto">
         <div className="lg:max-w-[80ch]">
           <div className="flex justify-start lg:justify-center">
@@ -35,5 +36,26 @@ export default function index() {
         </div>
       </div>
     </>
+  )
+}
+function BackgroundPattern() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="1440" height="560" className=" absolute inset-0 -z-10 h-full w-full opacity-30 blur-3xl rotate-3" preserveAspectRatio="none" viewBox="0 0 1440 560">
+      <g clip-path="url(&quot;#SvgjsClipPath1083&quot;)" fill="none">
+        <circle r="37.68" cx="27.69" cy="559.87" fill="#3997a1"></circle>
+        <circle r="54.375" cx="1157.82" cy="301.1" fill="#3997a1"></circle>
+        <circle r="49.095" cx="118.57" cy="478.95" fill="#3997a1"></circle>
+        <circle r="47.185" cx="855.46" cy="393.61" fill="#3997a1"></circle>
+        <circle r="52.19" cx="757.34" cy="80.44" fill="#3997a1"></circle>
+        <circle r="19.915" cx="1339.78" cy="479.78" fill="#3997a1"></circle>
+        <circle r="38.625" cx="1212.62" cy="548.11" fill="#3997a1"></circle>
+        <circle r="41.635" cx="1147.41" cy="187.11" fill="#3997a1"></circle>
+      </g>
+      <defs>
+        <clipPath id="SvgjsClipPath1083">
+          <rect width="1440" height="560" x="0" y="0"></rect>
+        </clipPath>
+      </defs>
+    </svg>
   )
 }
