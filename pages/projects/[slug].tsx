c61c42f19ec55e56501ccf6b980e99fb6f802eb0
Justin Bento <bento.a.justin@gmail.com>
2023-07-28 15:47:13 -0400

    added blog data from sanity

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -1,9 +1,31 @@
+import { Button } from '@/components/Button';
+import RichTextComponents from '@/components/RichTextComponents';
 import { client } from '@/sanityClient';
+import { PortableText } from '@portabletext/react';
+import Link from 'next/link';
 import React from 'react'
 
-export default function DynamicProjectPage() {
+export default function DynamicProjectPage({ post }: any) {
   return (
-    <div>[slug]</div>
+    <article className="md:container md:mx-auto lg:max-w-[100ch] lg:my-32 p-4 lg:p-0">
+      <nav>
+        <nav className="flex text-sm mb-2 items-center">
+          <Button variant="link" className="text-slate-700"><Link href="/">Home</Link></Button>
+          <span>/</span>
+          <Button variant="link" className="text-slate-700"><Link href="/projects">Projects</Link></Button>
+          <span>/</span>
+          <Button variant="link"><Link href={`/${post.title}`}>{post.title}</Link></Button>
+        </nav>
+      </nav>
+      <header>
+        <h1 className="block tracking-normal [text-wrap:balance] text-3xl sm:text-4xl font-extrabold md:text-5xl text-neutral-950">{post.title}</h1>
+        <p className=" mt-4 tracking-wide text-base sm:text-lg lg:text-xl text-neutral-600 max-w-[90ch]">{post.description}</p>
+        <div className="prose max-w-[100ch] prose-p:leading-7 prose-p:text-lg prose-p:tracking-wide">
+          <PortableText value={post.body} components={RichTextComponents} />
+        </div>
+      </header>
+
+    </article>
   )
 }
 
