5d41b83d959bedbebfd9afb714c1640608022e59
Justin Bento <bento.a.justin@gmail.com>
2023-07-28 20:21:55 -0400

    fixed up some typography

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -14,22 +14,20 @@ export default function DynamicProjectPage({ post }: any) {
         <meta name="title" content={`Justin Bento - Projects - ${post.title}`} />
         <meta name="description" content="Join me on an journey from multimedia design to web development. Explore interactive digital media with a passion for coding. " />
       </Head>
-      <article className="md:container md:mx-auto border-0 lg:max-w-[100ch] lg:my-32 p-4 lg:p-0">
-        <nav>
-          <nav className="flex text-sm mb-2 items-center">
-            <Button variant="link" className="text-slate-700"><Link href="/">Home</Link></Button>
-            <span>/</span>
-            <Button variant="link" className="text-slate-700"><Link href="/projects">Projects</Link></Button>
-            <span>/</span>
-            <Button variant="link" className="text-primary-600"><Link href={`/${post.title}`}>{post.title}</Link></Button>
-          </nav>
+      <main className="md:container md:mx-auto border-0 lg:max-w-[100ch] lg:my-32 p-4 lg:p-0">
+        <nav className="flex items-center mb-2 text-sm">
+          <Button variant="link" className="text-slate-700"><Link href="/">Home</Link></Button>
+          <span>/</span>
+          <Button variant="link" className="text-slate-700"><Link href="/projects">Projects</Link></Button>
+          <span>/</span>
+          <Button variant="link" className="text-primary-600"><Link href={`/${post.title}`}>{post.title}</Link></Button>
         </nav>
-        <header>
-          <h1 className="block tracking-normal [text-wrap:balance] text-3xl sm:text-4xl font-extrabold md:text-5xl">{post.title}</h1>
-          <p className="mt-4 tracking-wide text-base sm:text-lg lg:text-xl max-w-[90ch]">{post.description}</p>
-        </header>
-        <PortableText value={post.body} components={RichTextComponents} />
-      </article>
+        <h1 className="block tracking-normal [text-wrap:balance] text-3xl sm:text-4xl font-extrabold md:text-5xl">{post.title}</h1>
+        <p className="mt-4 tracking-wide text-base sm:text-lg lg:text-xl max-w-[90ch]">{post.description}</p>
+        <div className="max-w-full prose prose-p:leading-7 prose-p:text-lg prose-ul:text-lg prose-li:text-lg prose-p:tracking-wide prose-headings:font-bold prose-img:block">
+          <PortableText value={post.body} components={RichTextComponents} />
+        </div>
+      </main>
     </>
   )
 }
