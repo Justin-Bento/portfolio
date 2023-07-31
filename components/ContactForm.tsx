3c4b01199b9940f74825d79dbfbe8005b294d4db
Justin Bento <bento.a.justin@gmail.com>
2023-07-30 21:33:42 -0400

    fixed contact form for darkmode

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -22,12 +22,12 @@ export default function ContactForm() {
     <>
       <form onSubmit={handleSubmit(sendToFormspree)} method="POST" className="">
         <div className="lg:max-w-3xl">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
             <div>
               <label
                 htmlFor="first-name"
                 defaultValue="First_Name"
-                className="text-sm font-bold tracking-wide capitalize"
+                className="text-sm font-bold capitalize"
               >
                 First name
               </label>
@@ -37,7 +37,7 @@ export default function ContactForm() {
                   {...register("firstName")}
                   id="first-name"
                   autoComplete="given-name"
-                  className="flex h-10 w-full rounded-md border border-input bg-white/10 px-3 py-2 text-sm border-offset-slate-200 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-2 focus-visible:border-primary-300 focus-visible:border-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                  className="flex w-full h-10 px-3 py-2 text-sm border rounded-md border-input dark:bg-primary-950/10 dark:caret-primary-50 bg-white/10 dark:carrot-primary-200 dark:focus-visible:border-primary-700 dark:border-primary-700 file:border-0 file:bg-transparent file:text-sm file:font-medium dark:placeholder:text-primary-40 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-2 focus-visible:border-primary-300 focus-visible:border-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                 />
               </div>
             </div>
@@ -45,7 +45,7 @@ export default function ContactForm() {
               <label
                 htmlFor="last-name"
                 defaultValue="Last_Name"
-                className="text-sm font-bold tracking-wide capitalize"
+                className="text-sm font-bold capitalize"
               >
                 Last name
               </label>
@@ -55,14 +55,14 @@ export default function ContactForm() {
                   {...register("lastName")}
                   id="last-name"
                   autoComplete="family-name"
-                  className="flex h-10 w-full rounded-md border border-input bg-white/10 px-3 py-2 text-sm border-offset-slate-200 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-2 focus-visible:border-primary-300 focus-visible:border-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                  className="flex w-full h-10 px-3 py-2 text-sm border rounded-md border-input dark:bg-primary-950/10 dark:caret-primary-50 bg-white/10 dark:carrot-primary-200 dark:focus-visible:border-primary-700 dark:border-primary-700 file:border-0 file:bg-transparent file:text-sm file:font-medium dark:placeholder:text-primary-40 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-2 focus-visible:border-primary-300 focus-visible:border-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                 />
               </div>
             </div>
             <div className="sm:col-span-2">
               <label
                 htmlFor="email"
-                className="text-sm font-bold tracking-wide capitalize"
+                className="text-sm font-bold capitalize"
               >
                 Email
               </label>
@@ -72,11 +72,11 @@ export default function ContactForm() {
                   id="email"
                   autoComplete="email"
                   {...register("Email", { required: true })}
-                  className="flex h-10 w-full rounded-md border border-input bg-white/10 px-3 py-2 text-sm border-offset-slate-200 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-2 focus-visible:border-primary-300 focus-visible:border-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                  className="flex w-full h-10 px-3 py-2 text-sm border rounded-md border-input dark:bg-primary-950/10 dark:caret-primary-50 bg-white/10 dark:carrot-primary-200 dark:focus-visible:border-primary-700 dark:border-primary-700 file:border-0 file:bg-transparent file:text-sm file:font-medium dark:placeholder:text-primary-40 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-2 focus-visible:border-primary-300 focus-visible:border-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                 />
               </div>
               {errors.Email && (
-                <span className="text-xs capitalize font-normal tracking-wider text-red-800">
+                <span className="text-xs font-normal tracking-wider text-red-800 capitalize">
                   * Email field is required
                 </span>
               )}
@@ -84,7 +84,7 @@ export default function ContactForm() {
             <div className="sm:col-span-2">
               <label
                 htmlFor="message"
-                className="text-sm font-bold tracking-wide capitalize"
+                className="text-sm font-bold capitalize"
               >
                 Message
               </label>
@@ -92,11 +92,11 @@ export default function ContactForm() {
                 <textarea
                   id="message"
                   rows={4}
-                  className="flex min-h-[80px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm border-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-2 focus-visible:border-primary-300 focus-visible:border-ring focus-visible:border-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                  className="flex w-full h-10 px-3 py-2 text-sm min-h-[80px] border rounded-md border-input dark:bg-primary-950/10 dark:caret-primary-50 bg-white/10 dark:carrot-primary-200 dark:focus-visible:border-primary-700 dark:border-primary-700 file:border-0 file:bg-transparent file:text-sm file:font-medium dark:placeholder:text-primary-40 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-2 focus-visible:border-primary-300 focus-visible:border-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                   {...register("Message", { required: true })}
                 />
                 {errors.Message && (
-                  <span className="text-xs capitalize font-normal tracking-wider text-red-800">
+                  <span className="text-xs font-normal tracking-wider text-red-800 capitalize">
                     * Message field is required
                   </span>
                 )}
