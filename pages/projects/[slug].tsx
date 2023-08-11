9c33dd2e209e42898dd82d535859c5b1d28a09be
Justin Bento <bento.a.justin@gmail.com>
2023-08-11 15:08:10 -0400

    fixed spacing between elements

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -24,7 +24,7 @@ export default function DynamicProjectPage({ post }: any) {
         </nav>
         <h1 className="block tracking-normal [text-wrap:balance] text-3xl sm:text-4xl font-extrabold md:text-5xl">{post.title}</h1>
         <p className="mt-4 tracking-wide text-base sm:text-lg lg:text-xl max-w-[90ch]">{post.description}</p>
-        <div className="max-w-full prose prose-p:leading-7 dark:prose-invert prose-p:text-lg prose-ul:text-lg prose-li:text-lg prose-p:tracking-wide prose-headings:font-bold prose-img:block">
+        <div className="max-w-full space-y-8 prose-p:leading-7 dark:prose-invert prose-p:text-lg prose-ul:text-lg prose-li:text-lg prose-p:tracking-wide prose-headings:font-bold prose-img:block">
           <PortableText value={post.body} components={RichTextComponents} />
         </div>
       </main>
