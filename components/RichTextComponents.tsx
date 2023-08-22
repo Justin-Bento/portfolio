a2760637a24826477496a838c816253b2d70b76f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-08-21 23:57:46 -0400

    removed margin from image container

--- components/RichTextComponents.tsx
+++ components/RichTextComponents.tsx
@@ -6,7 +6,7 @@ const RichTextComponents = {
     image: ({ value }: any) => {
       return (
         <>
-          <div className="relative w-full mb-4 aspect-[4/3]">
+          <div className="relative w-full aspect-[4/3]">
             <Image
               fill
               src={urlFor(value).url()}
