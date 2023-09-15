d0beaa41ac221efa14fd21b85edb08e775c86975
Justin Bento <bento.a.justin@gmail.com>
2023-09-15 12:06:54 -0400

    changed falback path from flase to blocking

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -42,7 +42,7 @@ export async function getStaticPaths() {
     })) || [];
   return {
     paths,
-    fallback: false,
+    fallback: 'blocking',
   };
 }
 export async function getStaticProps({ params }: any) {
