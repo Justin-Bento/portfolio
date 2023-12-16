bafdf1052b59ff02a7275524529e217465a2ae7f
Justin Bento <bento.a.justin@gmail.com>
2023-12-16 14:00:04 -0500

    formatted document with new characterr limit

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,11 +1,8 @@
-
-import ThankYouPage from "@/layout/ThankYou"
-import {
-  useForm
-} from "react-hook-form";
-import { useForm as useFormspree } from "@formspree/react";
-import { Button } from "./Button";
-import { RiSendPlane2Line } from "react-icons/ri";
+import ThankYouPage from '@/layout/ThankYou';
+import { useForm } from 'react-hook-form';
+import { useForm as useFormspree } from '@formspree/react';
+import { Button } from './Button';
+import { RiSendPlane2Line } from 'react-icons/ri';
 
 type FormInputs = {
   firstName: string;
@@ -16,25 +13,27 @@ type FormInputs = {
 
 export default function ContactForm() {
   const [serverState, sendToFormspree] = useFormspree(`${process.env.NEXT_PUBLIC_FORM_DATA}`); // replace
-  const { register, handleSubmit, formState: { errors }, } = useForm<FormInputs>();
-  if (serverState.succeeded) { return <ThankYouPage /> }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormInputs>();
+  if (serverState.succeeded) {
+    return <ThankYouPage />;
+  }
   return (
     <>
       <form onSubmit={handleSubmit(sendToFormspree)} method="POST" className="">
         <div className="lg:max-w-3xl">
           <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
             <div>
-              <label
-                htmlFor="first-name"
-                defaultValue="First_Name"
-                className="text-sm font-bold capitalize"
-              >
+              <label htmlFor="first-name" defaultValue="First_Name" className="text-sm font-bold capitalize">
                 First name
               </label>
               <div className="mt-2.5">
                 <input
                   type="text"
-                  {...register("firstName")}
+                  {...register('firstName')}
                   id="first-name"
                   autoComplete="given-name"
                   className="flex w-full h-10 px-3 py-2 text-sm border rounded-md dark:text-neutral-300 border-input dark:bg-primary-950/10 dark:caret-primary-50 bg-white/10 dark:carrot-primary-200 dark:focus-visible:border-primary-700 dark:border-primary-700 file:border-0 file:bg-transparent file:text-sm file:font-medium dark:placeholder:text-primary-40 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-2 focus-visible:border-primary-300 focus-visible:border-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
@@ -42,17 +41,13 @@ export default function ContactForm() {
               </div>
             </div>
             <div>
-              <label
-                htmlFor="last-name"
-                defaultValue="Last_Name"
-                className="text-sm font-bold capitalize"
-              >
+              <label htmlFor="last-name" defaultValue="Last_Name" className="text-sm font-bold capitalize">
                 Last name
               </label>
               <div className="mt-2.5">
                 <input
                   type="text"
-                  {...register("lastName")}
+                  {...register('lastName')}
                   id="last-name"
                   autoComplete="family-name"
                   className="flex w-full h-10 px-3 py-2 text-sm border rounded-md dark:text-neutral-300 border-input dark:bg-primary-950/10 dark:caret-primary-50 bg-white/10 dark:carrot-primary-200 dark:focus-visible:border-primary-700 dark:border-primary-700 file:border-0 file:bg-transparent file:text-sm file:font-medium dark:placeholder:text-primary-40 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-2 focus-visible:border-primary-300 focus-visible:border-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
@@ -60,10 +55,7 @@ export default function ContactForm() {
               </div>
             </div>
             <div className="sm:col-span-2">
-              <label
-                htmlFor="email"
-                className="text-sm font-bold capitalize"
-              >
+              <label htmlFor="email" className="text-sm font-bold capitalize">
                 Email
               </label>
               <div className="mt-2.5">
@@ -71,7 +63,7 @@ export default function ContactForm() {
                   type="email"
                   id="email"
                   autoComplete="email"
-                  {...register("Email", { required: true })}
+                  {...register('Email', { required: true })}
                   className="flex w-full h-10 px-3 py-2 text-sm border rounded-md dark:text-neutral-300 border-input dark:bg-primary-950/10 dark:caret-primary-50 bg-white/10 dark:carrot-primary-200 dark:focus-visible:border-primary-700 dark:border-primary-700 file:border-0 file:bg-transparent file:text-sm file:font-medium dark:placeholder:text-primary-40 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-2 focus-visible:border-primary-300 focus-visible:border-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                 />
               </div>
@@ -82,10 +74,7 @@ export default function ContactForm() {
               )}
             </div>
             <div className="sm:col-span-2">
-              <label
-                htmlFor="message"
-                className="text-sm font-bold capitalize"
-              >
+              <label htmlFor="message" className="text-sm font-bold capitalize">
                 Message
               </label>
               <div className="mt-2.5">
@@ -93,7 +82,7 @@ export default function ContactForm() {
                   id="message"
                   rows={4}
                   className="flex w-full h-10 px-3 py-2 text-sm min-h-[80px] border dark:text-neutral-300 rounded-md border-input dark:bg-primary-950/10 dark:caret-primary-50 bg-white/10 dark:carrot-primary-200 dark:focus-visible:border-primary-700 dark:border-primary-700 file:border-0 file:bg-transparent file:text-sm file:font-medium dark:placeholder:text-primary-40 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:border-2 focus-visible:border-primary-300 focus-visible:border-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-                  {...register("Message", { required: true })}
+                  {...register('Message', { required: true })}
                 />
                 {errors.Message && (
                   <span className="text-xs font-normal tracking-wider text-red-800 dark:text-red-400 capitalize">
@@ -105,18 +94,13 @@ export default function ContactForm() {
           </div>
           <div className="mt-8">
             {/* errors will return when field validation fails  */}
-            <Button
-              type="submit"
-              variant="secondary"
-              className="gap-2.5"
-            >
+            <Button type="submit" variant="secondary" className="gap-2.5">
               <RiSendPlane2Line />
               Send message
             </Button>
           </div>
         </div>
       </form>
-
     </>
-  )
+  );
 }
