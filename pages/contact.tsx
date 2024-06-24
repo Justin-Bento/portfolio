35f75cb727fc634f3ff7b1ce82f9030a6aba30a5
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-06-24 17:22:40 -0400

    Updated border to have an accessible contrast ratio

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -18,34 +18,34 @@ export default function Contact() {
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
           <section className="lg:col-span-2">
             <nav className="flex items-center mb-2 text-sm">
-              <Button variant="link" className="text-slate-700 dark:text-slate-300"><Link href="/">Home</Link></Button>
-              <span className="dark:text-slate-300">/</span>
+              <Button variant="link" className="text-slate-700 dark:text-primary-40"><Link href="/">Home</Link></Button>
+              <span className="dark:text-primary-40">/</span>
               <Button variant="link" className="text-primary-600 dark:text-primary-400"><Link href="/contact">Contact us</Link></Button>
             </nav>
-            <h1 className="scroll-m-20 text-5xl font-extrabold tracking-tight capitalize">Let&#39;s work together!</h1>
+            <h1 className="scroll-m-20 text-5xl font-extrabold tracking-tight capitalize dark:text-primary-40">Let&#39;s work together!</h1>
             <p className="mt-4 prose lg:prose-lg max-w-full dark:text-primary-40">I&#39;d love to hear from you! Whether you have a project in mind, a collaboration opportunity, or just want to say hello, feel free to reach out. Fill in the form below, and I&#39;ll get back to you as soon as possible. Looking forward to starting a creative conversation together!</p>
           </section>
-          <div className="overflow-hidden border rounded-lg dark:border-slate-700">
-            <div className="px-4 py-5 sm:p-6">
-              <h2 className="text-base font-semibold capitalize font-display dark:text-slate-300">My Office</h2>
-              <p className="mt-2 text-base dark:text-slate-300">I have to list my location for legal reasons.</p>
-              <Button variant="ghost" className="inline-flex items-center gap-2 mt-2 dark:text-slate-300">
+          <div className="overflow-hidden border rounded-lg dark:border-slate-50">
+            <div className="px-4 py-5 sm:p-6 ">
+              <h2 className="text-base font-semibold capitalize font-display dark:text-primary-40">My Office</h2>
+              <p className="mt-2 text-base dark:text-primary-40">I have to list my location for legal reasons.</p>
+              <Button variant="ghost" className="inline-flex items-center gap-2 mt-2 ">
                 <RiMapPin2Fill />
                 Toronto Ontario
               </Button>
-              <hr className="my-6 border dark:border-slate-600" />
-              <h2 className="text-base font-semibold capitalize font-display dark:text-slate-300">Email Me</h2>
-              <Button variant="ghost" className="inline-flex items-center gap-2 mt-2 normal-case dark:text-slate-300">
+              <hr className="my-6 border dark:border-primary-100" />
+              <h2 className="text-base font-semibold capitalize font-display ">Email Me</h2>
+              <Button variant="ghost" className="inline-flex items-center gap-2 mt-2 normal-case ">
                 <RiMailFill />
                 contact@justinbento.com
               </Button>
-              <hr className="my-6 border dark:border-slate-600" />
-              <h2 className="text-base font-semibold capitalize font-display dark:text-slate-300">Social&#39;s</h2>
+              <hr className="my-6 border dark:border-primary-100" />
+              <h2 className="text-base font-semibold capitalize font-display ">Social&#39;s</h2>
               <div className="flex items-center gap-2 mt-4">
                 {footer_navigation.map((item: any) => (
                   <Link key={item.name} href={item.href} className="transition-all hover:opacity-80" target="_blank" aria-label="external-link-to-social-media">
                     <span className="sr-only">{item.name}</span>
-                    <Button variant="outline" size="icon">
+                    <Button variant="outline" size="icon" className="border-primary-50">
                       <item.icon className="w-6 h-6" aria-hidden="true" />
                     </Button>
                   </Link>
