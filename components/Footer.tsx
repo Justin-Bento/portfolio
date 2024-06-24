311bf916687f11e6ff9af0cff7f22ce8214f57b5
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-06-24 17:36:52 -0400

    Improved footer background to ba AA contrast

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,18 +4,18 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="dark:border-t dark:border-primary-100">
+    <footer className="bg-primary-800 dark:border-t dark:border-primary-100">
       <div className="px-4 py-10 lg:py-12 md:px-4 lg:mx-auto lg:container md:flex md:items-center md:justify-between">
         <div className="flex justify-start space-x-6 lg:justify-center md:order-2">
           {footer_navigation.map((item: any) => (
             <Link key={item.name} href={item.href} className="transition-all hover:opacity-80" target="_blank" aria-label="external-link-to-social-media">
               <span className="sr-only">{item.name}</span>
-              <item.icon className="w-6 h-6 dark:fill-primary-100/80" aria-hidden="true" />
+              <item.icon className="w-6 h-6 fill-primary-40 dark:fill-primary-100/80" aria-hidden="true" />
             </Link>
           ))}
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
-          <p className="text-sm leading-7 tracking-wider dark:text-primary-100">
+          <p className="text-sm text-primary-40 leading-7 dark:text-primary-100">
             &copy; {new Date().getFullYear()} Built and designed by Justin Bento.
           </p>
         </div>
