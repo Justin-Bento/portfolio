9edb6cbfc17b592e2aafb1855129edb104e8a08a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-08-08 17:01:20 -0400

    Removed tailwindjs file and moved to typescript.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    // ...
   ],
   prefix: "",
   theme: {
@@ -18,6 +20,26 @@ const config = {
       },
     },
     extend: {
+      colors: {
+        primary: {
+          40: "#f4f9f9",
+          50: "#f1fafa",
+          100: "#dbf1f2",
+          200: "#aedfe2",
+          300: "#8bcfd5",
+          400: "#54b3bc",
+          500: "#3997a1",
+          600: "#327b88",
+          700: "#2e6570",
+          800: "#2d545d",
+          900: "#294750",
+          950: "#172e35",
+          960: "#141819",
+        },
+      },
+      fontFamily: {
+        sans: ["Switzer", ...defaultTheme.fontFamily.sans],
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -27,14 +49,19 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        fadeIn: "fadeIn 1s linear",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
