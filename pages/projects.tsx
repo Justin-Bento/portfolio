c0c0c1df5668cda607d1c3381afb1af11b9f7017
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-08-09 13:29:15 -0400

    Updated theme to fixes darkmode inconsistencies

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -7,7 +7,12 @@ import Image from "next/image";
 import Link from "next/link";
 import Head from "next/head";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import {
   Breadcrumb,
   BreadcrumbList,
@@ -32,13 +37,19 @@ export default function Projects({ posts }: any) {
           <Breadcrumb>
             <BreadcrumbList>
               <BreadcrumbItem>
-                <BreadcrumbLink href="/" className="font-medium text-black">
+                <BreadcrumbLink
+                  href="/"
+                  className="font-medium text-black dark:text-white"
+                >
                   Home
                 </BreadcrumbLink>
               </BreadcrumbItem>
               <BreadcrumbSeparator />
               <BreadcrumbItem>
-                <BreadcrumbLink href="/Projects" className="font-medium text-primary">
+                <BreadcrumbLink
+                  href="/Projects"
+                  className="font-medium text-primary dark:text-accent"
+                >
                   Projects
                 </BreadcrumbLink>
               </BreadcrumbItem>
@@ -48,16 +59,21 @@ export default function Projects({ posts }: any) {
             A Showcase of Creativity
           </h1>
           <p className="leading-7 lg:text-lg text-balance max-w-5xl mt-4 dark:text-white">
-            Welcome to my gallery! On this page&#44; you&#39;ll find a collection of my projects throughout my career as
-            a website developer&#46; If you have any questions or would like to discuss a potential project or
-            collaboration&#44; don&#39;t hesitate to reach out to me&#46;
+            Welcome to my gallery! On this page&#44; you&#39;ll find a
+            collection of my projects throughout my career as a website
+            developer&#46; If you have any questions or would like to discuss a
+            potential project or collaboration&#44; don&#39;t hesitate to reach
+            out to me&#46;
           </p>
         </span>
         <div className="grid grid-cols-1 gap-8 mt-8 lg:grid-cols-2">
           {posts.map((post: any) => {
             return (
               <>
-                <Card key={post._id} className="bg-white ring-1 ring-black space-y-4 pt-6">
+                <Card
+                  key={post._id}
+                  className="bg-white ring-1 ring-black dark:ring-white space-y-4 pt-6"
+                >
                   <CardHeader className="relative flex w-8 h-8 overflow-hidden rounded-full shadow shrink-0 mx-6">
                     <Image
                       fill
@@ -67,18 +83,28 @@ export default function Projects({ posts }: any) {
                     />
                   </CardHeader>
                   <CardContent className="space-y-2">
-                    <h2 className="text-2xl font-bold tracking-tight capitalize">{post.title}</h2>
+                    <h2 className="text-2xl font-bold tracking-tight capitalize">
+                      {post.title}
+                    </h2>
                     <p className="leading-7 line-clamp-2">{post.description}</p>
                   </CardContent>
                   <CardFooter className="flex items-center gap-4">
                     <Link href={`/projects/${post.slug.current}`}>
-                      <Button variant="outline" size="sm" className="flex items-center gap-1">
+                      <Button
+                        variant="outline"
+                        size="sm"
+                        className="flex items-center gap-1 dark:border-white"
+                      >
                         <RiComputerLine />
                         View Project
                       </Button>
                     </Link>
                     <Link href={post.projectCode} target="_blank">
-                      <Button variant="outline" size="sm" className="flex items-center gap-1">
+                      <Button
+                        variant="outline"
+                        size="sm"
+                        className="flex items-center gap-1 dark:border-white"
+                      >
                         <RiCodeBoxLine />
                         View Code
                       </Button>
