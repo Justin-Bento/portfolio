e610d102f2377b38156b5183cfe1c4d1a5ace0f4
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-08-09 13:26:39 -0400

    Updated theme to fixes darkmode inconsistencies for the home

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,22 +31,31 @@ export default function index() {
             </span>
           </div>
           <div className="text-balance space-y-4">
-            <p className="text-base leading-7 tracking-wide capitalize dark:text-white">Hey there I&#39;m Justin!</p>
+            <p className="text-base leading-7 tracking-wide capitalize dark:text-white">
+              Hey there I&#39;m Justin!
+            </p>
             <h1 className="text-5xl text-pretty font-extrabold tracking-tight dark:text-white">
               From Designs to Dynamic Websites: Join Me On My Coding Adventure!
             </h1>
             <p className="text-base leading-7 tracking-wide lg:text-lg max-w-3xl mx-auto dark:text-white">
-              I&#39;m a Multimedia Designer venturing into the field of Front-End Development. My goal is to blend my
-              knowledge of accessibility design with the power of code to create engaging websites that everyone can
-              enjoy. Join me on this journey of creativity and interactivity!
+              I&#39;m a Multimedia Designer venturing into the field of
+              Front-End Development. My goal is to blend my knowledge of
+              accessibility design with the power of code to create engaging
+              websites that everyone can enjoy. Join me on this journey of
+              creativity and interactivity!
             </p>
           </div>
           <div className="flex items-center justify-center gap-4 mt-8">
             <Link href="/projects">
-              <Button variant="primary">Explore My Portfolio</Button>
+              <Button variant="primary" className="dark:text-white">
+                Explore My Portfolio
+              </Button>
             </Link>
             <Link href="/contact">
-              <Button variant="outline" className="ring-1 ring-black hover:underline">
+              <Button
+                variant="outline"
+                className="ring-1 ring-black hover:underline"
+              >
                 Let&#39;s Collaborate
               </Button>
             </Link>
