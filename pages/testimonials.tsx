f47f821b0bbc7a88eeac6d5b7a40f4ac923d9c78
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-08-23 13:56:07 -0500

    Included Head tag to include seo page for nextjs

--- pages/testimonials.tsx
+++ pages/testimonials.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Head from "next/head";
 
 export default function testimonials() {
   return (
     <>
+      <Head>
+        <title>Testimonials - Justin Bento</title>
+      </Head>
       <main className="container mx-auto [min-height:100dvh;] py-16 px-">
         <section className="">
           <h1 className="text-4xl font-bold dark:text-white">Testimonials</h1>
