ce2e8c19606a3295419342dc055f44da2e7b1aea
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-08-23 13:58:17 -0500

    fragemented seo from website code

--- pages/testimonials.tsx
+++ pages/testimonials.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function testimonials() {
   return (
     <>
-      <Head>
-        <title>Testimonials - Justin Bento</title>
-      </Head>
+    <SEO_Testimonials />
       <main className="container mx-auto [min-height:100dvh;] py-16 px-">
         <section className="">
           <h1 className="text-4xl font-bold dark:text-white">Testimonials</h1>
         </section>
       </main>
     </>
+  );
+}
+
+function SEO_Testimonials() {
+  return (
+    <Head>
+      <title>Testimonials - Justin Bento</title>
+    </Head>
   );
 }
