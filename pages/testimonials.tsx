eebd55a9baa1fefc1cdb24832656f9e0f2d28c45
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-08-23 13:52:28 -0500

    created a testimonials page with default container and heading

---
+++ pages/testimonials.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function testimonials() {
+  return (
+    <main className="container mx-auto [min-height:100dvh;] py-16 px-">
+      <section className="">
+        <h1 className="text-4xl font-bold dark:text-white">Testimonials</h1>
+      </section>
+    </main>
+  )
+}
