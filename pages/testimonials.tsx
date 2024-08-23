e6ba21c07183a382f7df0dd9b85ecc68d868d31a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-08-23 13:53:44 -0500

    Wrapped jsx elements inside a react fragment.
    Formatted document on save.

--- pages/testimonials.tsx
+++ pages/testimonials.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 export default function testimonials() {
   return (
-    <main className="container mx-auto [min-height:100dvh;] py-16 px-">
-      <section className="">
-        <h1 className="text-4xl font-bold dark:text-white">Testimonials</h1>
-      </section>
-    </main>
-  )
+    <>
+      <main className="container mx-auto [min-height:100dvh;] py-16 px-">
+        <section className="">
+          <h1 className="text-4xl font-bold dark:text-white">Testimonials</h1>
+        </section>
+      </main>
+    </>
+  );
 }
