8d603cc289a234e6555505c2d95e38f0f1e861e7
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-09-11 14:54:00 -0400

    Removed react fragmented

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -69,49 +69,47 @@ export default function Projects({ posts }: any) {
         <div className="grid grid-cols-1 gap-8 mt-8 lg:grid-cols-2">
           {posts.map((post: any) => {
             return (
-              <>
-                <Card
-                  key={post._id}
-                  className="bg-white dark:bg-black ring-1 ring-black dark:ring-white space-y-4 pt-6"
-                >
-                  <CardHeader className="relative flex w-8 h-8 overflow-hidden rounded-full shadow shrink-0 mx-6">
-                    <Image
-                      fill
-                      src={urlFor(post.mainImage).url()}
-                      alt={`Image of ${post.title.toLowerCase()}`}
-                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                    />
-                  </CardHeader>
-                  <CardContent className="space-y-2">
-                    <h2 className="text-2xl font-bold tracking-tight capitalize">
-                      {post.title}
-                    </h2>
-                    <p className="leading-7 line-clamp-2">{post.description}</p>
-                  </CardContent>
-                  <CardFooter className="flex items-center gap-4">
-                    <Link href={post.liveDemo}>
-                      <Button
-                        variant="outline"
-                        size="sm"
-                        className="flex items-center gap-1 dark:border-white"
-                      >
-                        <RiComputerLine />
-                        View Project
-                      </Button>
-                    </Link>
-                    <Link href={post.projectCode}>
-                      <Button
-                        variant="outline"
-                        size="sm"
-                        className="flex items-center gap-1 dark:border-white"
-                      >
-                        <RiCodeBoxLine />
-                        View Code
-                      </Button>
-                    </Link>
-                  </CardFooter>
-                </Card>
-              </>
+              <Card
+                key={post._id}
+                className="bg-white dark:bg-black ring-1 ring-black dark:ring-white space-y-4 pt-6"
+              >
+                <CardHeader className="relative flex w-8 h-8 overflow-hidden rounded-full shadow shrink-0 mx-6">
+                  <Image
+                    fill
+                    src={urlFor(post.mainImage).url()}
+                    alt={`Image of ${post.title.toLowerCase()}`}
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  />
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <h2 className="text-2xl font-bold tracking-tight capitalize">
+                    {post.title}
+                  </h2>
+                  <p className="leading-7 line-clamp-2">{post.description}</p>
+                </CardContent>
+                <CardFooter className="flex items-center gap-4">
+                  <Link href={post.liveDemo}>
+                    <Button
+                      variant="outline"
+                      size="sm"
+                      className="flex items-center gap-1 dark:border-white"
+                    >
+                      <RiComputerLine />
+                      View Project
+                    </Button>
+                  </Link>
+                  <Link href={post.projectCode}>
+                    <Button
+                      variant="outline"
+                      size="sm"
+                      className="flex items-center gap-1 dark:border-white"
+                    >
+                      <RiCodeBoxLine />
+                      View Code
+                    </Button>
+                  </Link>
+                </CardFooter>
+              </Card>
             );
           })}
         </div>
