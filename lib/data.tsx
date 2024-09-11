f8ee0fe0cf5ea51cd277b330cd6ab2b0bec7c4c9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-09-11 13:40:02 -0400

    Updated footer navigation aria lable with distinguishable button names

--- lib/data.tsx
+++ lib/data.tsx
@@ -6,21 +6,25 @@ export const footer_navigation = [
   {
     name: 'SoundCloud',
     href: 'https://soundcloud.com/bento-box-519166246',
+    ariaLabel: "SoundCloud External Link",
     icon: (props:any) => <RiSoundcloudFill {...props} />
   },
   {
     name: 'Github',
     href: 'https://github.com/Justin-Bento',
+    ariaLabel: "GitHub External Link",
     icon: (props:any) => <RiGithubFill {...props} />
   },
   {
     name: 'Pintrest',
     href: 'https://www.pinterest.ca/bentoajustin/',
+    ariaLabel: "Pintrest External Link",
     icon: (props:any) => <RiPinterestFill {...props} />
   },
   {
     name: 'LinkedIn',
     href: 'https://www.linkedin.com/in/justin--bento/',
+    ariaLabel: "LinedIn External Link",
     icon: (props:any) => <RiLinkedinBoxFill {...props} />
   },
 ]
