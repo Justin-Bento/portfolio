4f16f71dc8894ec847dffc79d7a4ca7401035884
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-24 09:59:12 -0400

    Changed Background Pattern from home page into a component.

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import type { Metadata } from "next";
 import Header from "@/components/Header";
+import BackgroundPattern from "@/components/BackgroundPattern";
 
 // import { Button } from "@/components/ui/button";
 
@@ -57,34 +58,5 @@ export default function index() {
         </div>
       </main>
     </>
-  );
-}
-function BackgroundPattern() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      version="1.1"
-      width="1440"
-      height="560"
-      className="absolute inset-0 w-full h-full -z-10 opacity-30 dark:opacity-40 blur-3xl"
-      preserveAspectRatio="none"
-      viewBox="0 0 1440 560"
-    >
-      <g>
-        <circle r="37.68" cx="27.69" cy="559.87" fill="#3997a1"></circle>
-        <circle r="54.375" cx="1157.82" cy="301.1" fill="#3997a1"></circle>
-        <circle r="49.095" cx="118.57" cy="478.95" fill="#3997a1"></circle>
-        <circle r="47.185" cx="855.46" cy="393.61" fill="#3997a1"></circle>
-        <circle r="52.19" cx="757.34" cy="80.44" fill="#3997a1"></circle>
-        <circle r="19.915" cx="1339.78" cy="479.78" fill="#3997a1"></circle>
-        <circle r="38.625" cx="1212.62" cy="548.11" fill="#3997a1"></circle>
-        <circle r="41.635" cx="1147.41" cy="187.11" fill="#3997a1"></circle>
-      </g>
-      <defs>
-        <clipPath id="svgClipPath1083">
-          <rect width="1440" height="560" x="0" y="0"></rect>
-        </clipPath>
-      </defs>
-    </svg>
   );
 }
