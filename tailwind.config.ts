c0ca877e80b35b6384387529660de738cc87d850
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-24 10:49:26 -0400

    Added tailwind colours in config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,79 +1,42 @@
 import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
 import colors from "tailwindcss/colors";
+import animate from "tailwindcss-animate";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	colors: {
-  		white: 'var(--black)',
-  		black: 'var(--white)',
-  		primary: 'var(--primary)',
-  		secondary: 'var(--secondary)',
-  		accent: 'var(--accent)',
-  		gray: 'colors.gray',
-  		red: 'colors.red',
-  		transparent: 'colors.transparent',
-  		current: 'colors.current'
-  	},
-  	extend: {
-  		fontFamily: {
-  			sans: ["Switzer", ...defaultTheme.fontFamily.sans]
-  		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    colors: {
+      black: "#040606",
+      white: "#fafcfc",
+      primary: "#2e6570",
+      secondary: "#83C6C6",
+      accent: "#6edede",
+      gray: colors.gray,
+      red: colors.red,
+      transparent: "transparent",
+      current: "currentColor",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Switzer", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {},
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    animate,
+    //...
+  ],
 };
 export default config;
