91616b5c9be9192728347d1eaff380fd17f90af4
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-24 11:19:49 -0400

    Created projects page and removed the file inside pages folder

---
+++ app/projects/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import type { Metadata } from "next";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+export const metadata: Metadata = {
+  title: "Justin Bento - Projects",
+  description:
+    "Join me on an journey from multimedia design to web development. Explore interactive digital media with a passion for coding.",
+};
+
+export default function page() {
+  return (
+    <>
+      <Header />
+      <main className="min-h-dvh container px-6 mx-auto my-24">
+        <h1 className="">Projects, Page.</h1>
+      </main>
+      <Footer />
+    </>
+  );
+}
