08cacfb44a56d84528ae5d31ceaf445e03b4ade2
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-24 12:40:16 -0400

    Transferred layout  and updated sanity import with the latest version of nextjs.

--- app/(frontend)/projects/page.tsx
+++ app/(frontend)/projects/page.tsx
@@ -1,20 +1,99 @@
-import React from "react";
-import type { Metadata } from "next";
-import Footer from "@/components/Footer";
+import Link from "next/link";
+import { client } from "@/sanity/lib/client";
+import { POSTS_QUERY } from "@/sanity/lib/queries";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import Image from "next/image";
+import { urlFor } from "@/sanity/lib/image";
+import { Button } from "@/components/ui/button";
+import { RiComputerLine } from "react-icons/ri";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
-export const metadata: Metadata = {
-  title: "Justin Bento - Projects",
-  description:
-    "Join me on an journey from multimedia design to web development. Explore interactive digital media with a passion for coding.",
+const options = { next: { revalidate: 60 } };
+
+type PostArticle = {
+  _id: string;
+  slug: { current: string };
+  description: string;
+  title: string;
+  mainImage: string;
+  projectCode: string;
+  liveDemo: string;
 };
 
-export default function page() {
+export default async function Page() {
+  const posts = await client.fetch(POSTS_QUERY, {}, options);
   return (
     <>
       <Header />
-      <main className="min-h-dvh container px-6 mx-auto my-24">
-        <h1 className="">Projects, Page.</h1>
+      <main className="container mx-auto grid grid-cols-1 gap-6 p-12 my-12 space-y-4">
+        <section className="space-y-4">
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/" className="font-medium text-black dark:text-white">
+                  Home
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/Projects" className="font-medium text-primary dark:text-accent">
+                  Projects
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+          <h1 className="text-4xl lg:text-5xl capitalize tracking-tight font-extrabold dark:text-white">
+            A Showcase of Creativity
+          </h1>
+          <p className="leading-7 lg:text-lg text-balance max-w-5xl mt-4 dark:text-white">
+            Welcome to my gallery! On this page&#44; you&#39;ll find a collection of my projects throughout my career as
+            a website developer&#46; If you have any questions or would like to discuss a potential project or
+            collaboration&#44; don&#39;t hesitate to reach out to me&#46;
+          </p>
+        </section>
+        <section className="grid lg:grid-cols-12 gap-8 ">
+          {posts.map((post: PostArticle) => (
+            <Card
+              key={post._id}
+              className="bg-white dark:bg-black ring-1 ring-black dark:ring-white space-y-4 pt-6 col-span-12 md:col-span-6"
+            >
+              <CardHeader className="relative flex w-8 h-8 overflow-hidden rounded-full shadow shrink-0 mx-6">
+                <Image
+                  fill
+                  src={urlFor(post.mainImage).url()}
+                  alt={`Image of ${post.title.toLowerCase()}`}
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                />
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <h2 className="text-2xl font-bold tracking-tight capitalize">{post.title}</h2>
+                <p className="leading-7 line-clamp-2">{post.description}</p>
+              </CardContent>
+              <CardFooter className="flex items-center gap-4">
+                <Link href={post.liveDemo}>
+                  <Button variant="outline" size="sm" className="flex items-center gap-1 dark:border-white">
+                    <RiComputerLine />
+                    View Project
+                  </Button>
+                </Link>
+                <Link href={post.projectCode}>
+                  <Button variant="outline" size="sm" className="flex items-center gap-1 dark:border-white">
+                    <RiComputerLine />
+                    View Code
+                  </Button>
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </section>
       </main>
       <Footer />
     </>
