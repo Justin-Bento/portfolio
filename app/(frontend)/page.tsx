a5c4a37bb979cec5519cc762b7a4f0a38dab986b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-02 12:28:26 -0400

    moved the spacing underneath the card element

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -6,48 +6,50 @@ export default function Home() {
   return (
     <>
       <main className="grid grid-cols-1 md:grid-cols-12 p-8 gap-6 max-h-dvh bg-[#fafafa]">
-        <Card className="bg-[#fff] p-5 ring-1 ring-black/5 shadow-none col-span-12">
-          <div className="flex items-center">
-            <span className="flex flex-1 items-center gap-4 justify-start">
-              <Logo />
-            </span>
-            <ul className="flex flex-1 items-center gap-4 justify-end">
-              <li>
-                <Link
-                  href="/"
-                  className="text-lg font-semibold tracking-[-0.015em] text-zinc-950 sm:text-base dark:text-white"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/"
-                  className="text-lg font-semibold tracking-[-0.015em] text-zinc-950 sm:text-base dark:text-white"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/"
-                  className="text-lg font-semibold tracking-[-0.015em] text-zinc-950 sm:text-base dark:text-white"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/"
-                  className="text-lg font-semibold tracking-[-0.015em] text-zinc-950 sm:text-base dark:text-white"
-                >
-                  Home
-                </Link>
-              </li>
-            </ul>
+        <Card className="ring-1 ring-black/5 shadow-none col-span-12">
+          <div className="px-4 py-5 sm:p-6">
+            <div className="flex items-center">
+              <span className="flex flex-1 items-center gap-4 justify-start">
+                <Logo />
+              </span>
+              <ul className="flex flex-1 items-center gap-4 justify-end">
+                <li>
+                  <Link
+                    href="/"
+                    className="text-lg font-semibold tracking-[-0.015em] text-zinc-950 sm:text-base dark:text-white"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/"
+                    className="text-lg font-semibold tracking-[-0.015em] text-zinc-950 sm:text-base dark:text-white"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/"
+                    className="text-lg font-semibold tracking-[-0.015em] text-zinc-950 sm:text-base dark:text-white"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/"
+                    className="text-lg font-semibold tracking-[-0.015em] text-zinc-950 sm:text-base dark:text-white"
+                  >
+                    Home
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </Card>
-        <Card className="bg-[#fff] ring-1 ring-black/5 shadow-none col-span-12 space-y-5">
+        <Card className="ring-1 ring-black/5 shadow-none col-span-12 space-y-5">
           <div className="px-4 py-5 sm:p-6">
             <h1 className="text-lg/7 font-semibold tracking-[-0.015em] text-zinc-950 sm:text-base/7 dark:text-white">
               Hi my name is,
@@ -56,28 +58,28 @@ export default function Home() {
             <p className="text-6xl font-bold text-black/50 dark:text-white">I build things for the web.</p>
           </div>
         </Card>
-        <Card className="bg-[#fff] ring-1 ring-black/5 shadow-none col-span-12 lg:col-span-4">
+        <Card className="ring-1 ring-black/5 shadow-none col-span-12 lg:col-span-4">
           <div className="px-4 py-5 sm:p-6">
             <h2 className="text-lg/7 font-semibold tracking-[-0.015em] text-zinc-950 sm:text-base/7 dark:text-white">
               About Me
             </h2>
           </div>
         </Card>
-        <Card className="bg-[#fff] ring-1 ring-black/5 shadow-none col-span-12 lg:col-span-4">
+        <Card className="ring-1 ring-black/5 shadow-none col-span-12 lg:col-span-4">
           <div className="px-4 py-5 sm:p-6">
             <h2 className="text-lg/7 font-semibold tracking-[-0.015em] text-zinc-950 sm:text-base/7 dark:text-white">
               Some Things I&#39;ve built.
             </h2>
           </div>
         </Card>
-        <Card className="bg-[#fff] ring-1 ring-black/5 shadow-none col-span-12 lg:col-span-4">
+        <Card className="ring-1 ring-black/5 shadow-none col-span-12 lg:col-span-4">
           <div className="px-4 py-5 sm:p-6">
             <h2 className="text-lg/7 font-semibold tracking-[-0.015em] text-zinc-950 sm:text-base/7 dark:text-white">
               Where I&#39;ve worked
             </h2>
           </div>
         </Card>
-        <Card className="bg-[#fff] ring-1 ring-black/5 shadow-none col-span-12">
+        <Card className="ring-1 ring-black/5 shadow-none col-span-12">
           <div className="px-4 py-5 sm:p-6">
             <h2 className="text-lg/7 font-semibold tracking-[-0.015em] text-zinc-950 sm:text-base/7 dark:text-white">
               Footer
