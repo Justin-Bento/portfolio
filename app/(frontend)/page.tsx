875ec6c539adfe95dc0b6a9eca473ad0cb5ba815
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-02 13:33:41 -0400

    Added social media icons with links.

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,15 +1,31 @@
 import { Card } from "@/components/ui/card";
+import Link from "next/link";
+import { RiGithubLine, RiLinkedinBoxLine, RiSoundcloudLine, RiTwitterLine } from "react-icons/ri";
 
 export default function Home() {
   return (
     <>
       <main className="grid grid-cols-1 md:grid-cols-12 gap-6 p-8 max-h-dvh bg-white">
         {/* Main Hero Section */}
-        <Card className="col-span-12 h-[50dvh] flex items-center justify-center">
+        <Card className="col-span-12 h-[65dvh] flex items-center justify-center">
           <div className="space-y-4 w-11/12 mx-auto">
             <p className="scroll-m-20 font-bold text-md">Hi, my name is</p>
             <h1 className="scroll-m-20 font-extrabold text-6xl">Justin Bento</h1>
             <p className="scroll-m-20 font-extrabold text-6xl opacity-70">I build things for the web.</p>
+            <div className="flex items-center gap-4">
+              <Link target="_blank" rel="noopener noreferrer" href="https://github.com/Justin-Bento">
+                <RiGithubLine className="size-6" />
+              </Link>
+              <Link target="_blank" rel="noopener noreferrer" href="https://github.com/Justin-Bento">
+                <RiLinkedinBoxLine className="size-6" />
+              </Link>
+              <Link target="_blank" rel="noopener noreferrer" href="https://github.com/Justin-Bento">
+                <RiTwitterLine className="size-6" />
+              </Link>
+              <Link target="_blank" rel="noopener noreferrer" href="https://github.com/Justin-Bento">
+                <RiSoundcloudLine className="size-6" />
+              </Link>
+            </div>
           </div>
         </Card>
         {/* About Me Card */}
