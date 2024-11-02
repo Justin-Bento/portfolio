ceca8b3062cd7350db61322903e7d3ebf63d105b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-02 13:16:16 -0400

    Deprecated footer card

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -8,28 +8,6 @@ export default function Home() {
   return (
     <>
       <main className="grid grid-cols-1 md:grid-cols-12 gap-6 p-8 max-h-dvh bg-white">
-        {/* Header Card */}
-        <Card className="col-span-12">
-          <div className="px-4 py-5 sm:p-6">
-            <div className="flex items-center">
-              <span className="flex flex-1 items-center gap-4 justify-start">
-                <Logo />
-              </span>
-              <ul className="flex flex-1 items-center gap-4 justify-end">
-                {navLinks.map((link) => (
-                  <li key={link}>
-                    <Link
-                      href={`/${link.toLowerCase()}`}
-                      className="text-lg font-semibold tracking-[-0.015em] text-zinc-950 sm:text-base dark:text-white"
-                    >
-                      {link}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </Card>
         {/* Main Hero Section */}
         <Card className="col-span-12 h-[50dvh] flex items-center justify-center">
           <div className="space-y-4 w-11/12 mx-auto">
