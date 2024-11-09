61a716fb25d2f3a35f0b85feb9d20e75b2ad227f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-08 20:07:35 -0500

    fixed large display

--- app/(frontend)/about/page.tsx
+++ app/(frontend)/about/page.tsx
@@ -14,101 +14,103 @@ export default function Page() {
   return (
     <>
       <Navigation />
-      <main className="grid grid-cols-1 md:grid-cols-12 gap-4 p-4 lg:gap-6 lg:p-8 3xl:container 3xl:mx-auto">
-        {/* Full-width Card */}
-        <Card className="col-span-12 bg-secondary/10 border-transparent">
-          <div className="min-h-[65svh] flex items-center w-11/12 mx-auto ">
-            <div className="space-y-3">
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem>
-                    <BreadcrumbLink href="/" className="font-medium text-black/50 dark:text-white">
-                      Home
-                    </BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator />
-                  <BreadcrumbItem>
-                    <BreadcrumbLink href="/about" className="font-medium text-black">
-                      About
-                    </BreadcrumbLink>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
-              <h2 className="text-3xl lg:text-5xl font-extrabold capitalize dark:text-white">
-                Hello, allow me to introduce myself.
-              </h2>
-              <p className="text-balance text-black opacity-70 line-clamp-2">
-                You are inside a Bento Grid, a layout inspired by the traditional Japanese bento box, where items are
-                arranged in a clean, organized, and visually pleasing way. Each section in this grid is like a carefully
-                placed item within a bento box, designed to create harmony and balance.
-              </p>
+      <main className="p-4 lg:gap-6 lg:p-8 3xl:container 3xl:mx-auto">
+        <section className="grid grid-cols-1 md:grid-cols-12 gap-4">
+          {/* Full-width Card */}
+          <Card className="col-span-12 bg-secondary/10 border-transparent">
+            <div className="min-h-[65svh] xl:min-h-[36rem] flex items-center w-11/12 mx-auto ">
+              <div className="space-y-3">
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    <BreadcrumbItem>
+                      <BreadcrumbLink href="/" className="font-medium text-black/50 dark:text-white">
+                        Home
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                      <BreadcrumbLink href="/about" className="font-medium text-black">
+                        About
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                  </BreadcrumbList>
+                </Breadcrumb>
+                <h2 className="text-3xl lg:text-5xl font-extrabold capitalize dark:text-white">
+                  Hello, allow me to introduce myself.
+                </h2>
+                <p className="text-balance text-black opacity-70 line-clamp-2">
+                  You are inside a Bento Grid, a layout inspired by the traditional Japanese bento box, where items are
+                  arranged in a clean, organized, and visually pleasing way. Each section in this grid is like a
+                  carefully placed item within a bento box, designed to create harmony and balance.
+                </p>
+              </div>
             </div>
-          </div>
-        </Card>
+          </Card>
 
-        {/* 7-column Card (left-aligned) */}
-        <Card className="col-span-12 md:col-span-7">
-          <div className="min-h-[37vh] flex items-center w-11/12 mx-auto">
-            <div className="space-y-2">
-              <h2 className="text-xl capitalize font-semibold tracking-wide leading-normal">
-                Hello, this is a summary of my development background
-              </h2>
-              <p className="text-balance text-black opacity-70 line-clamp-2">
-                You are inside a Bento Grid, a layout inspired by the traditional Japanese bento box, where items are
-                arranged in a clean, organized, and visually pleasing way. Each section in this grid is like a carefully
-                placed item within a bento box, designed to create harmony and balance.
-              </p>
+          {/* 7-column Card (left-aligned) */}
+          <Card className="col-span-12 md:col-span-7">
+            <div className="min-h-[37vh] flex items-center w-11/12 mx-auto">
+              <div className="space-y-2">
+                <h2 className="text-xl capitalize font-semibold tracking-wide leading-normal">
+                  Hello, this is a summary of my development background
+                </h2>
+                <p className="text-balance text-black opacity-70 line-clamp-2">
+                  You are inside a Bento Grid, a layout inspired by the traditional Japanese bento box, where items are
+                  arranged in a clean, organized, and visually pleasing way. Each section in this grid is like a
+                  carefully placed item within a bento box, designed to create harmony and balance.
+                </p>
+              </div>
             </div>
-          </div>
-        </Card>
+          </Card>
 
-        {/* 5-column Card (right-aligned) */}
-        <Card className="col-span-12 md:col-span-5">
-          <div className="min-h-[37vh] flex items-center w-11/12 mx-auto">
-            <div className="space-y-2">
-              <h2 className="text-xl capitalize font-semibold tracking-wide leading-normal">
-                Hello, this is a summary of my design background!
-              </h2>
-              <p className="text-balance text-black opacity-70 line-clamp-2">
-                You are inside a Bento Grid, a layout inspired by the traditional Japanese bento box, where items are
-                arranged in a clean, organized, and visually pleasing way. Each section in this grid is like a carefully
-                placed item within a bento box, designed to create harmony and balance.
-              </p>
+          {/* 5-column Card (right-aligned) */}
+          <Card className="col-span-12 md:col-span-5">
+            <div className="min-h-[37vh] flex items-center w-11/12 mx-auto">
+              <div className="space-y-2">
+                <h2 className="text-xl capitalize font-semibold tracking-wide leading-normal">
+                  Hello, this is a summary of my design background!
+                </h2>
+                <p className="text-balance text-black opacity-70 line-clamp-2">
+                  You are inside a Bento Grid, a layout inspired by the traditional Japanese bento box, where items are
+                  arranged in a clean, organized, and visually pleasing way. Each section in this grid is like a
+                  carefully placed item within a bento box, designed to create harmony and balance.
+                </p>
+              </div>
             </div>
-          </div>
-        </Card>
+          </Card>
 
-        {/* 5-column Card (left-aligned) */}
-        <Card className="col-span-12 md:col-span-5">
-          <div className="min-h-[37vh] flex items-center w-11/12 mx-auto">
-            <div className="space-y-2">
-              <h2 className="text-xl capitalize font-semibold tracking-wide leading-normal">
-                Hello, this is the About page!
-              </h2>
-              <p className="text-balance text-black opacity-70 line-clamp-2">
-                You are inside a Bento Grid, a layout inspired by the traditional Japanese bento box, where items are
-                arranged in a clean, organized, and visually pleasing way. Each section in this grid is like a carefully
-                placed item within a bento box, designed to create harmony and balance.
-              </p>
+          {/* 5-column Card (left-aligned) */}
+          <Card className="col-span-12 md:col-span-5">
+            <div className="min-h-[37vh] flex items-center w-11/12 mx-auto">
+              <div className="space-y-2">
+                <h2 className="text-xl capitalize font-semibold tracking-wide leading-normal">
+                  Hello, this is the About page!
+                </h2>
+                <p className="text-balance text-black opacity-70 line-clamp-2">
+                  You are inside a Bento Grid, a layout inspired by the traditional Japanese bento box, where items are
+                  arranged in a clean, organized, and visually pleasing way. Each section in this grid is like a
+                  carefully placed item within a bento box, designed to create harmony and balance.
+                </p>
+              </div>
             </div>
-          </div>
-        </Card>
+          </Card>
 
-        {/* 7-column Card (right-aligned) */}
-        <Card className="col-span-12 md:col-span-7">
-          <div className="min-h-[37vh] flex items-center w-11/12 mx-auto">
-            <div className="space-y-2">
-              <h2 className="text-xl capitalize font-semibold tracking-wide leading-normal">
-                Hello, this is a summary me as a person!
-              </h2>
-              <p className="text-balance text-black opacity-70 line-clamp-2">
-                You are inside a Bento Grid, a layout inspired by the traditional Japanese bento box, where items are
-                arranged in a clean, organized, and visually pleasing way. Each section in this grid is like a carefully
-                placed item within a bento box, designed to create harmony and balance.
-              </p>
+          {/* 7-column Card (right-aligned) */}
+          <Card className="col-span-12 md:col-span-7">
+            <div className="min-h-[37vh] flex items-center w-11/12 mx-auto">
+              <div className="space-y-2">
+                <h2 className="text-xl capitalize font-semibold tracking-wide leading-normal">
+                  Hello, this is a summary me as a person!
+                </h2>
+                <p className="text-balance text-black opacity-70 line-clamp-2">
+                  You are inside a Bento Grid, a layout inspired by the traditional Japanese bento box, where items are
+                  arranged in a clean, organized, and visually pleasing way. Each section in this grid is like a
+                  carefully placed item within a bento box, designed to create harmony and balance.
+                </p>
+              </div>
             </div>
-          </div>
-        </Card>
+          </Card>
+        </section>
       </main>
       <Footer />
     </>
