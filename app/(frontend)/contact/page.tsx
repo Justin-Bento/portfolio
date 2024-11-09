f1cc7284d87879e12a083027cb9901a84e493db6
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-08 20:11:37 -0500

    fixed up html

--- app/(frontend)/contact/page.tsx
+++ app/(frontend)/contact/page.tsx
@@ -19,75 +19,77 @@ export default function page() {
   return (
     <>
       <Navigation />
-      <main className="grid grid-cols-1 md:grid-cols-12 gap-4  p-4 lg:p-8 3xl:container 3xl:mx-auto">
-        <Card className="min-h-[65svh] flex items-center justify-center col-span-12 bg-[#c6afb1]/50 border-transparent">
-          <div className="px-4 py-5 sm:p-6 space-y-2">
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem>
-                  <BreadcrumbLink href="/" className="font-medium text-black dark:text-white">
-                    Home
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                  <BreadcrumbLink href="/Projects" className="font-medium text-primary dark:text-accent">
-                    Contact
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-            <h1 className="text-3xl lg:text-5xl font-extrabold tracking-tight capitalize dark:text-white">
-              Let&#39;s work together!
-            </h1>
-            <p className="text-sm/5 md:text-base/7 w-8/12 text-balance">
-              I&#39;d love to hear from you! Whether you have a project in mind, a collaboration opportunity, or just
-              want to say hello, feel free to reach out. Fill in the form below, and I&#39;ll get back to you as soon as
-              possible. Looking forward to starting a creative conversation!
-            </p>
-          </div>
-        </Card>
-        <Card className="min-h-[20dvh] col-span-12 md:col-span-6">
-          <div className="px-4 py-5 sm:p-6 grid lg:grid-cols-1 gap-16 my-16">
-            <div id="contact-information" className="overflow-hidden">
-              <h2 className="text-base font-semibold capitalize font-display dark:text-primary-40">My Office</h2>
-              <p className="mt-2 text-base dark:text-primary-40">I have to list my location for legal reasons.</p>
-              <Button variant="ghost" className="inline-flex items-center gap-2 mt-2 ">
-                <RiMapPin2Line />
-                Toronto Ontario
-              </Button>
-              <hr className="my-6 border-1 border-black/20 dark:border-gray-50" />
-              <h2 className="text-base font-semibold capitalize font-display ">Email Me</h2>
-              <Button variant="ghost" className="inline-flex items-center gap-2 mt-2 normal-case ">
-                <RiMailLine />
-                contact@justinbento.com
-              </Button>
-              <hr className="my-6 border-1 border-black/20 dark:border-gray-50" />
-              <h2 className="text-base font-semibold capitalize font-display ">Social&#39;s</h2>
-              <div className="flex items-center gap-2 mt-4">
-                {footer_navigation.map((item: IconProps) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className="transition-all hover:opacity-80"
-                    target="_blank"
-                    aria-label="external-link-to-social-media"
-                  >
-                    <span className="sr-only">{item.name}</span>
-                    <Button variant="outline" size="icon" className="border-primary-50">
-                      <item.icon className="w-6 h-6" aria-hidden="true" />
-                    </Button>
-                  </Link>
-                ))}
+      <main className="p-4 lg:p-8 3xl:container 3xl:mx-auto">
+        <section className="grid grid-cols-1 md:grid-cols-12 gap-4">
+          <Card className="min-h-[65svh] xl:min-h-[36rem] flex items-center justify-center col-span-12 bg-[#c6afb1]/20 border-transparent">
+            <div className="px-4 py-5 sm:p-6 space-y-2">
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="/" className="font-medium text-black dark:text-white">
+                      Home
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="/Projects" className="font-medium text-primary dark:text-accent">
+                      Contact
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+              <h1 className="text-3xl lg:text-5xl font-extrabold tracking-tight capitalize dark:text-white">
+                Let&#39;s work together!
+              </h1>
+              <p className="text-sm/5 md:text-base/7 w-8/12 text-balance">
+                I&#39;d love to hear from you! Whether you have a project in mind, a collaboration opportunity, or just
+                want to say hello, feel free to reach out. Fill in the form below, and I&#39;ll get back to you as soon
+                as possible. Looking forward to starting a creative conversation!
+              </p>
+            </div>
+          </Card>
+          <Card className="min-h-[20dvh] col-span-12 md:col-span-6">
+            <div className="px-4 py-5 sm:p-6 grid lg:grid-cols-1 gap-16 my-16">
+              <div id="contact-information" className="overflow-hidden">
+                <h2 className="text-base font-semibold capitalize font-display dark:text-primary-40">My Office</h2>
+                <p className="mt-2 text-base dark:text-primary-40">I have to list my location for legal reasons.</p>
+                <Button variant="ghost" className="inline-flex items-center gap-2 mt-2 ">
+                  <RiMapPin2Line />
+                  Toronto Ontario
+                </Button>
+                <hr className="my-6 border-1 border-black/20 dark:border-gray-50" />
+                <h2 className="text-base font-semibold capitalize font-display ">Email Me</h2>
+                <Button variant="ghost" className="inline-flex items-center gap-2 mt-2 normal-case ">
+                  <RiMailLine />
+                  contact@justinbento.com
+                </Button>
+                <hr className="my-6 border-1 border-black/20 dark:border-gray-50" />
+                <h2 className="text-base font-semibold capitalize font-display ">Social&#39;s</h2>
+                <div className="flex items-center gap-2 mt-4">
+                  {footer_navigation.map((item: IconProps) => (
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      className="transition-all hover:opacity-80"
+                      target="_blank"
+                      aria-label="external-link-to-social-media"
+                    >
+                      <span className="sr-only">{item.name}</span>
+                      <Button variant="outline" size="icon" className="border-primary-50">
+                        <item.icon className="w-6 h-6" aria-hidden="true" />
+                      </Button>
+                    </Link>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        </Card>
-        <Card className="min-h-[20dvh] col-span-12 md:col-span-6">
-          <div className="px-4 py-5 sm:p-6">
-            <ContactForm />
-          </div>
-        </Card>
+          </Card>
+          <Card className="min-h-[20dvh] col-span-12 md:col-span-6">
+            <div className="px-4 py-5 sm:p-6">
+              <ContactForm />
+            </div>
+          </Card>
+        </section>
       </main>
       <Footer />
     </>
--- app/(frontend)/projects/page.tsx
+++ app/(frontend)/projects/page.tsx
@@ -33,61 +33,65 @@ export default async function Page() {
   return (
     <>
       <Navigation />
-      <main className="grid grid-cols-1 md:grid-cols-12 gap-4 p-4 lg:gap-6 lg:p-8 3xl:container 3xl:mx-auto">
-        <section className="bg-[#aba388]/30 col-span-12 p-8 rounded-lg min-h-[65svh] flex items-center">
-          <div className="p-6 space-y-2">
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem>
-                  <BreadcrumbLink href="/" className="font-medium text-black/50 dark:text-white">
-                    Home
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                  <BreadcrumbLink href="/Projects" className="font-medium text-black">
-                    Projects
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-            <h1 className="text-4xl lg:text-5xl font-bold dark:text-white">A Showcase of Creativity.</h1>
-            <p className="leading-7 lg:text-lg text-balance max-w-5xl mt-4 dark:text-white">
-              Welcome to my gallery! On this page&#44; you&#39;ll find a collection of my projects throughout my career
-              as a website developer&#46; If you have any questions or would like to discuss a potential project or
-              collaboration&#44; don&#39;t hesitate to reach out to me&#46;
-            </p>
-          </div>
-        </section>
-        {posts.map((post: PostArticle) => (
-          <Card key={post._id} className="col-span-12 md:col-span-6 p-6">
-            <div className="relative w-10 h-10 mb-4">
-              <Image
-                fill
-                src={urlFor(post.mainImage).url()}
-                alt={`Image of ${post.title.toLowerCase()}`}
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                className="rounded-full"
-              />
+      <main className="p-4 lg:gap-6 lg:p-8 3xl:container 3xl:mx-auto">
+        <section className="grid grid-cols-1 md:grid-cols-12 gap-4">
+          <Card className="min-h-[65svh] xl:min-h-[36rem] bg-[#aba388]/20 col-span-12 p-8 rounded-lg flex items-center">
+            <div className="p-6 space-y-2">
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="/" className="font-medium text-black/50 dark:text-white">
+                      Home
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href="/Projects" className="font-medium text-black">
+                      Projects
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+              <h1 className="text-4xl lg:text-5xl font-bold dark:text-white">A Showcase of Creativity.</h1>
+              <p className="leading-7 lg:text-lg text-balance max-w-5xl mt-4 dark:text-white">
+                Welcome to my gallery! On this page&#44; you&#39;ll find a collection of my projects throughout my
+                career as a website developer&#46; If you have any questions or would like to discuss a potential
+                project or collaboration&#44; don&#39;t hesitate to reach out to me&#46;
+              </p>
             </div>
-            <h2 className="text-2xl font-bold text-gray-900 dark:text-white my-2">{post.title}</h2>
-            <p className="text-base font-medium text-gray-500 dark:text-gray-300 leading-relaxed">{post.description}</p>
-            <span className="block space-x-4 mt-4">
-              <Link href={post.projectCode}>
-                <Button variant="secondary" size="sm">
-                  <RiGithubLine />
-                  View Code on GitHub
-                </Button>
-              </Link>
-              <Link href={post.liveDemo}>
-                <Button variant="secondary" size="sm">
-                  <RiComputerLine />
-                  Interact With Project
-                </Button>
-              </Link>
-            </span>
           </Card>
-        ))}
+          {posts.map((post: PostArticle) => (
+            <Card key={post._id} className="col-span-12 md:col-span-6 p-6">
+              <div className="relative w-10 h-10 mb-4">
+                <Image
+                  fill
+                  src={urlFor(post.mainImage).url()}
+                  alt={`Image of ${post.title.toLowerCase()}`}
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  className="rounded-full"
+                />
+              </div>
+              <h2 className="text-2xl font-bold text-gray-900 dark:text-white my-2">{post.title}</h2>
+              <p className="text-base font-medium text-gray-500 dark:text-gray-300 leading-relaxed">
+                {post.description}
+              </p>
+              <span className="block space-x-4 mt-4">
+                <Link href={post.projectCode}>
+                  <Button variant="secondary" size="sm">
+                    <RiGithubLine />
+                    View Code on GitHub
+                  </Button>
+                </Link>
+                <Link href={post.liveDemo}>
+                  <Button variant="secondary" size="sm">
+                    <RiComputerLine />
+                    Interact With Project
+                  </Button>
+                </Link>
+              </span>
+            </Card>
+          ))}
+        </section>
       </main>
       <Footer />
     </>
