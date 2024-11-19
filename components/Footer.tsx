00a3a930129670d61a7ae1d44a391b3a1f31420f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-19 16:20:08 -0500

    Added list elements so <a> cannot be a descendant of <a>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,24 +14,26 @@ export default function Footer() {
   return (
     <footer className="dark:border-primary-100 bg-gray-200 dark:border-t dark:bg-transparent">
       <div className="px-8 py-16 md:flex md:items-center md:justify-between">
-        <div className="flex justify-start space-x-6 md:order-2 lg:justify-center">
+        <ul className="flex list-none justify-start space-x-6 md:order-2 lg:justify-center">
           {footer_navigation.map((item: FooterNavProps) => (
-            <Link
-              key={item.name}
-              href={item.href}
-              className="transition-all hover:opacity-80"
-              target="_blank"
-              aria-label={item.ariaLabel}
-            >
-              <span className="sr-only">{item.name}</span>
-              {/* Properly rendering the icon component */}
-              <item.icon
-                className="h-6 w-6 fill-black dark:fill-white/80"
-                aria-hidden="true"
-              />
-            </Link>
+            <li className="">
+              <Link
+                key={item.name}
+                href={item.href}
+                className="transition-all hover:opacity-80"
+                target="_blank"
+                aria-label={item.ariaLabel}
+              >
+                <span className="sr-only">{item.name}</span>
+                {/* Properly rendering the icon component */}
+                <item.icon
+                  className="h-6 w-6 fill-black dark:fill-white/80"
+                  aria-hidden="true"
+                />
+              </Link>
+            </li>
           ))}
-        </div>
+        </ul>
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-sm leading-7 text-black dark:text-white">
             &copy; {new Date().getFullYear()} Built and designed by Justin
