1f1434e115e4813591af36f8f98bf97d11112aca
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-08 21:12:20 -0500

    Fixed footer to utilize ShadCN components

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { footer_navigation } from "@/lib/data";
 import Link from "next/link";
+import { Card, CardContent, CardDescription } from "./ui/card";
 
 // Define the FooterNavProps interface correctly
 export interface FooterNavProps {
@@ -12,35 +13,37 @@ export interface FooterNavProps {
 
 export default function Footer() {
   return (
-    <footer className="dark:border-primary-100 bg-gray-200 dark:border-t dark:bg-transparent">
-      <div className="px-8 py-16 md:flex md:items-center md:justify-between">
-        <ul className="flex list-none justify-start space-x-6 md:order-2 lg:justify-center">
-          {footer_navigation.map((item: FooterNavProps) => (
-            <li className="">
-              <Link
-                key={item.name}
-                href={item.href}
-                className="transition-all hover:opacity-80"
-                target="_blank"
-                aria-label={item.ariaLabel}
-              >
-                <span className="sr-only">{item.name}</span>
-                {/* Properly rendering the icon component */}
-                <item.icon
-                  className="h-6 w-6 fill-black dark:fill-white/80"
-                  aria-hidden="true"
-                />
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <div className="mt-8 md:order-1 md:mt-0">
-          <p className="text-sm leading-7 text-black dark:text-white">
-            &copy; {new Date().getFullYear()} Built and designed by Justin
-            Bento.
-          </p>
-        </div>
-      </div>
+    <footer className="">
+      <Card className="dark:border-primary-100 border-none bg-gray-200 shadow-none dark:border-t dark:bg-transparent">
+        <CardContent className="px-8 py-16 md:flex md:items-center md:justify-between">
+          <ul className="flex list-none justify-start space-x-6 md:order-2 lg:justify-center">
+            {footer_navigation.map((item: FooterNavProps) => (
+              <li className="">
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  className="transition-all hover:opacity-80"
+                  target="_blank"
+                  aria-label={item.ariaLabel}
+                >
+                  <span className="sr-only">{item.name}</span>
+                  {/* Properly rendering the icon component */}
+                  <item.icon
+                    className="h-6 w-6 fill-black dark:fill-white/80"
+                    aria-hidden="true"
+                  />
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="mt-8 md:order-1 md:mt-0">
+            <CardDescription>
+              &copy; {new Date().getFullYear()} Built and designed by Justin
+              Bento.
+            </CardDescription>
+          </div>
+        </CardContent>
+      </Card>
     </footer>
   );
 }
