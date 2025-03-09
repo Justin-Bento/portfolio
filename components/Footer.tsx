b1fdc1516da55747997bf44e0a584c2910972387
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-09 12:27:59 -0400

    Moved key to toep level element within the block.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,6 +5,7 @@ import { Card, CardContent, CardDescription } from "./ui/card";
 
 // Define the FooterNavProps interface correctly
 export interface FooterNavProps {
+  index: number; // This is not needed in the interface since `index` is provided by `map`
   name: string;
   href: string;
   ariaLabel: string;
@@ -17,17 +18,15 @@ export default function Footer() {
       <Card className="dark:border-primary-100 border-none bg-gray-200 shadow-none dark:border-t dark:bg-transparent">
         <CardContent className="px-8 py-16 md:flex md:items-center md:justify-between">
           <ul className="flex list-none justify-start space-x-6 md:order-2 lg:justify-center">
-            {footer_navigation.map((item: FooterNavProps) => (
-              <li className="">
+            {footer_navigation.map((item, index) => (
+              <li key={index} className="">
                 <Link
-                  key={item.name.toString().toLowerCase()}
                   href={item.href}
                   className="transition-all hover:opacity-80"
                   target="_blank"
                   aria-label={item.ariaLabel}
                 >
                   <span className="sr-only">{item.name}</span>
-                  {/* Properly rendering the icon component */}
                   <item.icon
                     className="h-6 w-6 fill-black dark:fill-white/80"
                     aria-hidden="true"
