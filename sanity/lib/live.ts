7bda5f6424db47f45709ae1d75ce313a02db1518
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-16 19:19:27 -0400

    Removed all instansaces of older nextjs version.

--- sanity/lib/live.ts
+++
@@ -1,4 +0,0 @@
-import { defineLive } from "next-sanity";
-import { client } from "@/sanity/lib/client";
-
-export const { sanityFetch, SanityLive } = defineLive({client});
--- sanity/lib/queries.ts
+++
@@ -1,6 +0,0 @@
-import { defineQuery } from "next-sanity";
-
-export const POSTS_QUERY =
-  defineQuery(`*[_type == "post" && defined(slug.current)]{
-  _id, title, description, mainImage, liveDemo, projectCode
-}`);
