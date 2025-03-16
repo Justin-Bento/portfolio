d91344ec4e1e5500543cbb7e828eea67aa98bcad
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-16 19:42:14 -0400

    Updated font to switzer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const switzer = localFont({
+  src: "../../public/fonts/Switzer-Variable.ttf",
+  weight: "100 900",
+  display: "swap",
+  variable: "--font-switzer",
 });
 
 export const metadata: Metadata = {
@@ -24,11 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${switzer.className}  antialiased`}>{children}</body>
     </html>
   );
 }
