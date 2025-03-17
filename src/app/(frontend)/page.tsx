10701a5e1bf22742de7590179da49163eaf64c59
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-16 20:56:51 -0400

    Updated card title, description and

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,10 @@
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
 import Link from "next/link";
 import {
   RiGithubLine,
@@ -82,35 +87,33 @@ export default function Home() {
           {/* About Me Card */}
           <div className="group col-span-12 lg:col-span-4">
             <Link href="/about">
-              <Card className="flex h-[25vh] flex-col items-start justify-center space-y-3 px-4 py-5 transition-all group-hover:bg-accent/30 sm:p-6 xl:h-64">
-                <h2 className="text-md/7 font-semibold text-gray-950 dark:text-white">
-                  About Me
-                </h2>
-                <p className="line-clamp-4 text-base text-gray-700 sm:text-sm/6 dark:text-gray-400">
-                  Hello! My name is Justin, and I have a passion for creating
-                  things that live on the internet. My journey into web
-                  development began back in 2019 when I started experimenting
-                  with custom HTML, and CSS themes. What began as a casual
-                  interest in tweaking designs quickly turned into a fascination
-                  with the power of code. As I learned to manipulate HTML and
-                  CSS through computer classes, I realized that these tools
-                  allowed me to bring my ideas to life in ways I hadn&#39;t
-                  imagined.
-                </p>
-                <p className="flex items-center justify-start gap-1 text-sm text-gray-700 group-hover:underline dark:text-gray-400">
-                  Read More <span className="block text-base">&rarr;</span>
-                </p>
+              <Card className="group-hover:bg-accent/30">
+                <CardContent className="flex flex-col items-start justify-center space-y-3 transition-all xl:h-64 xl:space-y-6">
+                  <CardTitle>About Me</CardTitle>
+                  <CardDescription className="line-clamp-4 leading-6">
+                    Hello! My name is Justin, and I have a passion for creating
+                    things that live on the internet. My journey into web
+                    development began back in 2019 when I started experimenting
+                    with custom HTML, and CSS themes. What began as a casual
+                    interest in tweaking designs quickly turned into a
+                    fascination with the power of code. As I learned to
+                    manipulate HTML and CSS through computer classes, I realized
+                    that these tools allowed me to bring my ideas to life in
+                    ways I hadn&#39;t imagined.
+                  </CardDescription>
+                  <CardDescription className="flex items-center justify-start gap-1">
+                    Read More <span className="block text-base">&rarr;</span>
+                  </CardDescription>
+                </CardContent>
               </Card>
             </Link>
           </div>
           {/* Projects Card */}
-          <div className="group col-span-12 lg:col-span-4">
-            <Link href="/projects">
-              <Card className="flex h-[25vh] flex-col items-start justify-center space-y-3 px-4 py-5 transition-all group-hover:bg-[#aba388]/30 sm:p-6 xl:h-64">
-                <h2 className="text-md/7 font-semibold text-gray-950 dark:text-white">
-                  Some Things I&#39;ve Built
-                </h2>
-                <p className="line-clamp-4 text-base text-gray-700 sm:text-sm/6 dark:text-gray-400">
+          <Link href="/projects" className="group col-span-12 lg:col-span-4">
+            <Card className="group-hover:bg-accent/30">
+              <CardContent className="flex flex-col items-start justify-center space-y-3 transition-all xl:h-64 xl:space-y-6">
+                <CardTitle>Some Things I&#39;ve Built</CardTitle>
+                <CardDescription className="line-clamp-4 leading-6">
                   I love building interactive projects for the web because it
                   allows me to bring ideas to life in ways that are both
                   functional and engaging. Over the years, I&#39;ve had the
@@ -122,34 +125,32 @@ export default function Home() {
                   fundamentals are the foundation of great development, so I
                   regularly revisit the basics to sharpen my skills and ensure
                   my code is both clean and efficient.
-                </p>
-                <p className="flex items-center justify-start gap-1 text-sm text-gray-700 group-hover:underline dark:text-gray-400">
+                </CardDescription>
+                <CardDescription className="flex items-center justify-start gap-1">
                   Read More <span className="block text-base">&rarr;</span>
-                </p>
-              </Card>
-            </Link>
-          </div>
+                </CardDescription>
+              </CardContent>
+            </Card>
+          </Link>
           {/* Work Experience Card */}
-          <div className="group col-span-12 lg:col-span-4">
-            <Link href="/experience">
-              <Card className="flex h-[25vh] flex-col items-start justify-center space-y-3 px-4 py-5 transition-all group-hover:bg-[#c6afb1]/30 sm:p-6 xl:h-64">
-                <h2 className="text-md/7 font-semibold text-gray-950 dark:text-white">
-                  Where I&#39;ve Worked
-                </h2>
-                <p className="line-clamp-4 text-base text-gray-700 sm:text-sm/6 dark:text-gray-400">
+          <Link href="/experience" className="group col-span-12 lg:col-span-4">
+            <Card className="group-hover:bg-accent/30">
+              <CardContent className="flex flex-col items-start justify-center space-y-3 transition-all xl:h-64 xl:space-y-6">
+                <CardTitle>Where I&#39;ve Worked</CardTitle>
+                <CardDescription className="line-clamp-4 leading-6">
                   After graduating during the pandemic, I found myself at a
                   crossroads, exploring various career paths to find the right
                   fit. It was a challenging time, filled with uncertainty, but
                   it ultimately led me to the world of web development. I
                   quickly realized that it was something I genuinely enjoyed and
                   felt driven to pursue.
-                </p>
-                <p className="flex items-center justify-start gap-1 text-sm text-gray-700 group-hover:underline dark:text-gray-400">
+                </CardDescription>
+                <CardDescription className="flex items-center justify-start gap-1">
                   Read More <span className="block text-base">&rarr;</span>
-                </p>
-              </Card>
-            </Link>
-          </div>
+                </CardDescription>
+              </CardContent>
+            </Card>
+          </Link>
         </section>
       </main>
       <Footer />
