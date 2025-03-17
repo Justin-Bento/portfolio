199a17d9299f443a0d342d3ac5d21a504925e0aa
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-16 20:34:13 -0400

    enabled images to allow images from sanity and unsplash

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
