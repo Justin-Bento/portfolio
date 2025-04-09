9f1780939e205066fa739a01ab64e1f07943ca2f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-09 16:33:11 -0400

    Removed the responsive queiry

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -16,7 +16,7 @@ export default function FrontEndLayout({
         enableSystem
         disableTransitionOnChange
       >
-        <main className="w-screen lg:container mx-auto min-h-dvh p-4 lg:p-6">
+        <main className="w-screen container mx-auto min-h-dvh p-4 lg:p-6">
           {children}
         </main>
       </ThemeProvider>
