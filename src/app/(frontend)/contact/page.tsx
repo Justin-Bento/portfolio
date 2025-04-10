0e12625323891b3d4bc7fee407f77021b0584ef2
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-09 22:59:09 -0400

    Updated the bento grid

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -7,6 +7,7 @@ import ContactForm from "@/components/ContactForm";
 import { MailOpen, Map } from "lucide-react";
 import { Metadata } from "next";
 import FullWidthCard from "@/components/FullWidthCard";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Cotnact",
@@ -37,65 +38,79 @@ export default function page() {
             soon as possible.
           </FullWidthCard.Overview>
         </FullWidthCard>
-        <Card className="col-span-12 min-h-[20dvh] lg:col-span-6">
-          <div className="my-16 grid gap-16 px-4 py-5 sm:p-6 lg:grid-cols-1">
-            <div id="contact-information" className="overflow-hidden">
-              <h2 className="font-display dark:text-primary-40 text-base font-semibold capitalize">
-                My Office
-              </h2>
-              <p className="dark:text-primary-40 mt-2 text-base">
-                I have to list my location for legal reasons.
-              </p>
-              <Button
-                variant="ghost"
-                className="mt-2 inline-flex items-center gap-2"
-              >
-                <Map />
-                Toronto Ontario
-              </Button>
-              <hr className="border-1 my-6 border-black/20 dark:border-gray-50" />
-              <h2 className="font-display text-base font-semibold capitalize">
-                Email Me
-              </h2>
-              <Button
-                variant="ghost"
-                className="mt-2 inline-flex items-center gap-2 normal-case"
-              >
-                <MailOpen />
-                contact@justinbento.com
-              </Button>
-              <hr className="border-1 my-6 border-black/20 dark:border-gray-50" />
-              <h2 className="font-display text-base font-semibold capitalize">
-                Social&#39;s
-              </h2>
-              <div className="mt-4 flex items-center gap-2">
-                {footer_navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className="transition-all hover:opacity-80"
-                    target="_blank"
-                    aria-label="external-link-to-social-media"
-                  >
-                    <span className="sr-only">{item.name}</span>
-                    <Button
-                      variant="outline"
-                      size="icon"
-                      className="border-primary-50"
-                    >
-                      <item.icon className="h-6 w-6" aria-hidden="true" />
-                    </Button>
-                  </Link>
-                ))}
+        <div className="col-span-12 grid gap-4 lg:grid-cols-3 lg:grid-rows-2">
+          <div className="relative lg:row-span-2">
+            <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
+              <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
+                <p className="mt-2 text-lg font-medium text-foreground max-lg:text-center">
+                  Open for Collaborations
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-muted-foreground max-lg:text-center">
+                  I’m always open to new collaborations—Follow me on GitHub to
+                  collaborate with eachothers projects.
+                </p>
+              </div>
+              <div className="@container relative min-h-[30rem] w-full grow max-lg:mx-auto max-lg:max-w-sm">
+                <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
+                  <Image
+                    fill
+                    className="size-full object-cover object-top"
+                    src="/images/image-1.jpeg"
+                    alt=""
+                  />
+                </div>
               </div>
             </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 lg:rounded-l-[2rem]"></div>
           </div>
-        </Card>
-        <Card className="col-span-12 min-h-[20dvh] lg:col-span-6">
-          <div className="px-4 py-5 sm:p-6">
-            <ContactForm />
+          <div className="relative lg:row-span-2">
+            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                <p className="mt-2 text-lg font-medium text-foreground max-lg:text-center">
+                  Where in the World Am I?
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-muted-foreground max-lg:text-center">
+                  Currently anchored in
+                  <span className="text-teal-500 font-semibold">
+                    Toronto, ON
+                  </span>
+                  , but my ideas sail globally and I'm ready to help.
+                </p>
+              </div>
+              <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10">
+                <div className="relative w-full h-[25rem]">
+                  <Image
+                    fill
+                    className="w-full max-lg:max-w-xs rounded-3xl"
+                    src="/images/image-3.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-t-[2rem]"></div>
           </div>
-        </Card>
+          <div className="relative lg:row-span-2">
+            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+              <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
+                <p className="mt-2 text-lg font-medium text-foreground max-lg:text-center">
+                  Let's Get In Touch
+                </p>
+                <p className="mt-2 max-w-lg text-sm/6 text-muted-foreground max-lg:text-center">
+                  No bots here—just me. If you like what you see and want to
+                  work together, let’s make it happen.
+                </p>
+              </div>
+              <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
+                <ContactForm />
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+          </div>
+        </div>
       </section>
     </>
   );
