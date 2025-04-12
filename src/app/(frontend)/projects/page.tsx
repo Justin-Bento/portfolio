99beffc6820093554e47a5cb9991c8e04a2ab60c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-11 22:47:09 -0400

    Removed unused imports for th eproject page

--- src/app/(frontend)/projects/page.tsx
+++ src/app/(frontend)/projects/page.tsx
@@ -1,21 +1,10 @@
 import { sanityFetch } from "@/sanity/lib/live";
 import { POSTS_QUERY } from "@/sanity/lib/queries";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import Image from "next/image";
-import { urlFor } from "@/sanity/lib/image";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { Github, Laptop } from "lucide-react";
-
+import { GithubIcon, Laptop } from "lucide-react";
 import { Metadata } from "next";
 import FullWidthCard from "@/components/FullWidthCard";
+import ProjectCard from "@/components/ProjectCard";
+import { urlFor } from "@/sanity/lib/image";
 
 export const metadata: Metadata = {
   title: "Projects",
@@ -51,49 +40,30 @@ export default async function Page() {
       <ul className="grid list-none grid-cols-12 gap-6 pt-8">
         {posts.map((post) => (
           <li key={post._id} className="col-span-12 lg:col-span-6">
-            <Card className="">
-              <CardContent className="space-y-4">
-                <CardHeader className="relative mb-4 size-12">
-                  {post.mainImage ? (
-                    <Image
-                      fill
-                      src={urlFor(post.mainImage).url()}
-                      alt={`Image of ${post.title}`}
-                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                      className="rounded-full"
-                    />
-                  ) : null}
-                </CardHeader>
-                <CardTitle className="font-medium text-foreground">
-                  {post?.title}
-                </CardTitle>
-                <CardDescription className="mt-2 text-sm text-muted-foreground text-balance">
-                  {post?.description}
-                </CardDescription>
-                <CardFooter className="pl-0 flex flex-col lg:flex-row items-start gap-2">
-                  <Link href={`${post.projectCode}`}>
-                    <Button
-                      variant="secondary"
-                      size="sm"
-                      className="w-full text-xs"
-                    >
-                      <Github />
-                      View Code on GitHub
-                    </Button>
-                  </Link>
-                  <Link href={`${post.liveDemo}`}>
-                    <Button
-                      variant="secondary"
-                      size="sm"
-                      className="w-full text-xs"
-                    >
-                      <Laptop />
-                      Interact With Project
-                    </Button>
-                  </Link>
-                </CardFooter>
-              </CardContent>
-            </Card>
+            <ProjectCard.Layout>
+              {post.mainImage ? (
+                <ProjectCard.Media
+                  src={urlFor(post.mainImage).url()}
+                  alt={`Image of ${post.title}`}
+                />
+              ) : null}
+              <ProjectCard.Headline>{post.title}</ProjectCard.Headline>
+              <ProjectCard.Description>
+                {post.description}
+              </ProjectCard.Description>
+              <div className="flex items-center gap-4 mt-4">
+                <ProjectCard.SocialLink
+                  href={`${post.liveDemo}`}
+                  icon={Laptop}
+                  label="View Live Project"
+                />
+                <ProjectCard.SocialLink
+                  href={`${post.projectCode}`}
+                  icon={GithubIcon}
+                  label="View Code"
+                />
+              </div>
+            </ProjectCard.Layout>
           </li>
         ))}
       </ul>
