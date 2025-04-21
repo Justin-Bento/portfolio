5c56ad64416ac2c3e55762c754bd9c7c8862e402
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-21 13:03:28 -0400

    Applied wrapper component to card footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { footer_navigation } from "@/lib/data";
 import Link from "next/link";
 import { Card, CardContent, CardDescription } from "./ui/card";
+import Wrapper from "./Wrapper";
 
 // Define the FooterNavProps interface correctly
 export interface FooterNavProps {
@@ -16,31 +17,33 @@ export default function Footer() {
   return (
     <footer className="">
       <Card className="shadow-none rounded-none">
-        <CardContent className="container mx-auto p-6 md:flex md:items-center md:justify-between">
-          <ul className="flex list-none justify-start space-x-6 md:order-2 lg:justify-center">
-            {footer_navigation.map((item, index) => (
-              <li key={index} className="">
-                <Link
-                  href={item.href}
-                  className="transition-all hover:opacity-80"
-                  target="_blank"
-                  aria-label={item.ariaLabel}
-                >
-                  <span className="sr-only">{item.name}</span>
-                  <item.icon
-                    className="size-6 hover:border-primary/80"
-                    aria-hidden="true"
-                  />
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <div className="mt-8 md:order-1 md:mt-0">
-            <CardDescription className="text-foreground">
-              &copy; {new Date().getFullYear()} Built and designed by Justin
-              Bento.
-            </CardDescription>
-          </div>
+        <CardContent>
+          <Wrapper className="md:flex md:items-center md:justify-between">
+            <ul className="flex list-none justify-start space-x-6 md:order-2 lg:justify-center">
+              {footer_navigation.map((item, index) => (
+                <li key={index} className="">
+                  <Link
+                    href={item.href}
+                    className="transition-all hover:opacity-80"
+                    target="_blank"
+                    aria-label={item.ariaLabel}
+                  >
+                    <span className="sr-only">{item.name}</span>
+                    <item.icon
+                      className="size-6 hover:border-primary/80"
+                      aria-hidden="true"
+                    />
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <div className="mt-8 md:order-1 md:mt-0">
+              <CardDescription className="text-foreground">
+                &copy; {new Date().getFullYear()} Built and designed by Justin
+                Bento.
+              </CardDescription>
+            </div>
+          </Wrapper>
         </CardContent>
       </Card>
     </footer>
