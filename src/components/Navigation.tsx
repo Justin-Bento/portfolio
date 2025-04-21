ef12a6918d5ca7bdf14b3d921247914306e57840
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-21 13:16:00 -0400

    Removed the card component for header component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState } from "react";
-import { Card, CardContent, CardDescription } from "./ui/card";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { Logo } from "./Logo";
@@ -14,77 +13,69 @@ export default function Header() {
   const navigation = ["home", "about", "projects", "contact"];
 
   return (
-    <header className="sticky top-0 z-30">
-      <Card className="rounded-none p-0">
-        <CardContent className="py-0">
-          <Wrapper className="grid grid-cols-1 items-center gap-4 lg:grid-cols-3">
-            {/* Logo and Mobile Menu Toggle */}
-            <div className="flex w-full items-center justify-between">
-              <Link href="/">
-                <Logo className="fill-foreground hover:opacity-70 hover:cursor-pointer" />
-              </Link>
-              <Button
-                variant="outline"
-                size="icon"
-                className="lg:hidden dark:text-foreground"
-                onClick={toggleMobileMenu}
-                aria-label={
-                  mobileMenuOpen ? "nav-button-close" : "nav-button-open"
-                }
-              >
-                {mobileMenuOpen ? <X /> : <Menu />}
-              </Button>
-            </div>
+    <header className="sticky top-0 z-30 backdrop-blur-md bg-card/30 shadow-sm">
+      <Wrapper className="grid grid-cols-1 items-center gap-4 lg:grid-cols-3">
+        {/* Logo and Mobile Menu Toggle */}
+        <div className="flex w-full items-center justify-between">
+          <Link href="/">
+            <Logo className="fill-foreground hover:opacity-70 hover:cursor-pointer" />
+          </Link>
+          <Button
+            variant="outline"
+            size="icon"
+            className="lg:hidden dark:text-foreground"
+            onClick={toggleMobileMenu}
+            aria-label={mobileMenuOpen ? "nav-button-close" : "nav-button-open"}
+          >
+            {mobileMenuOpen ? <X /> : <Menu />}
+          </Button>
+        </div>
 
-            {/* Navigation Links */}
-            <div
-              className={`${
-                mobileMenuOpen ? "block" : "hidden"
-              } lg:block lg:flex lg:items-center lg:justify-center`}
-            >
-              <ul
-                role="list"
-                className="flex flex-col gap-4 lg:flex-row lg:items-center lg:justify-center"
-              >
-                {navigation.map((navigator, index) => (
-                  <li key={index}>
-                    <Link href={navigator === "home" ? "/" : `/${navigator}`}>
-                      <Button
-                        variant="link"
-                        className="w-full justify-start lg:justify-center"
-                      >
-                        <CardDescription className="capitalize text-foreground">
-                          {navigator}
-                        </CardDescription>
-                      </Button>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
+        {/* Navigation Links */}
+        <div
+          className={`${
+            mobileMenuOpen ? "block" : "hidden"
+          } lg:block lg:flex lg:items-center lg:justify-center`}
+        >
+          <ul
+            role="list"
+            className="flex flex-col gap-4 lg:flex-row lg:items-center lg:justify-center"
+          >
+            {navigation.map((navigator, index) => (
+              <li key={index}>
+                <Link href={navigator === "home" ? "/" : `/${navigator}`}>
+                  <Button
+                    variant="link"
+                    className="w-full justify-start lg:justify-center capitalize text-foreground hover:underline"
+                  >
+                    {navigator}
+                  </Button>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-            {/* External Link (Github) */}
-            <div
-              className={`${
-                mobileMenuOpen ? "block" : "hidden"
-              } lg:block lg:flex lg:items-center lg:justify-end`}
-            >
-              <Link
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://github.com/Justin-Bento"
-              >
-                <Button variant="secondary">
-                  <span className="inline-flex flex-row-reverse items-center gap-2">
-                    <ExternalLink className="hover:underline" />
-                    Github
-                  </span>
-                </Button>
-              </Link>
-            </div>
-          </Wrapper>
-        </CardContent>
-      </Card>
+        {/* External Link (Github) */}
+        <div
+          className={`${
+            mobileMenuOpen ? "block" : "hidden"
+          } lg:block lg:flex lg:items-center lg:justify-end`}
+        >
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/Justin-Bento"
+          >
+            <Button variant="secondary">
+              <span className="inline-flex flex-row-reverse items-center gap-2">
+                <ExternalLink className="hover:underline" />
+                Github
+              </span>
+            </Button>
+          </Link>
+        </div>
+      </Wrapper>
     </header>
   );
 }
