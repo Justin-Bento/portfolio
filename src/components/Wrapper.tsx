b482e84b4a9f1afa3aaa7790c62bece198428aa6
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-04-21 12:56:43 -0400

    Created a wrapper component ensuring the content is wrapped with the same width.

---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type ExampleProps = {
+  children?: React.ReactNode;
+  className?: string; // Optional className
+};
+
+export default function Wrapper({ children, className = "" }: ExampleProps) {
+  return (
+    <div
+      className={`w-screen xl:container mx-auto min-h-dvh p-4 md:p-6 ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
